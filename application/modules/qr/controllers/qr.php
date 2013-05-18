0308fd8c8d3f82b1c350836e28c757e081791d74
juanb <juanb@lmde>
2013-05-18 18:14:41 -0300

    Initial commit with supporting libraries
    https://github.com/dwisetiyadi/CodeIgniter-PHP-QR-Code
    
    and 
    
    https://github.com/dwa012/html5-qrcode
    Signed-off-by:juanb <juanb@lmde>

---
+++ application/modules/qr/controllers/qr.php
@@ -0,0 +1,68 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * qr
+ * 
+ * This class scans and generates qrcodes
+ * 
+ * @author Juan Ignacio Borda <juanignacioborda@gmail.com>
+ * @date    May 18, 2013
+ *  Level 
+ * Level L (Low) 	7% of codewords can be restored.
+ * Level M (Medium) 	15% of codewords can be restored.
+ * Level Q (Quartile)[33] 	25% of codewords can be restored.
+ * Level H (High) 	30% of codewords can be restored.
+ */
+class Qr extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('user');
+        $this->user->authorize();
+        $this->load->library('parser');
+        $this->load->library('ui');
+        //---base variables
+        $this->base_url = base_url();
+        $this->module_url = base_url() . 'qr/';
+        //----LOAD LANGUAGE
+        $this->idu = (float) $this->session->userdata('iduser');
+        //---config
+        $this->load->config('config');
+    }
+
+    /*
+     * Index
+     */
+
+    function Index() {
+        $this->gen('www.dna2.org');
+    }
+
+    function Get_demo() {
+        $cpData['base_url'] = $this->base_url;
+        $cpData['module_url'] = $this->module_url;
+        $cpData['title'] = $this->module_url;
+        $cpData['reader_title'] = $this->module_url;
+        $cpData['reader_subtitle'] = $this->module_url;
+        $cpData['js'] = array(
+            $this->module_url . "assets/jscript/html5-qrcode.min.js" => 'HTML5 qrcode',
+            $this->module_url . "assets/jscript/qr.js" => 'Main functions',
+        );
+        $this->ui->compose('readqr','bootstrap.ui.php',$cpData);
+    }
+
+    function Gen($data, $size = '9', $level = 'H') {
+        $this->load->library('ciqrcode');
+        $params['cachedir'] = 'application/modules/qr/cache/';
+        $params['errorlog'] = 'application/modules/qr/log/';        
+        $params['data'] = $data;
+        $params['level'] = $level;
+        $params['size'] = $size;
+        header("Content-Type: image/png");
+        $this->ciqrcode->generate($params);
+    }
+
+}
---
+++ application/modules/qr/views/readqr.php
@@ -0,0 +1,41 @@
+<!-- HEADER -->
+<div id="header_wrap" class="outer">
+    <header class="inner">
+        <h1 id="project_title">{reader_title}</h1>
+        <h2 id="project_tagline">{reader_subtitle}</h2>
+    </header>
+</div>
+
+<!-- MAIN CONTENT -->
+<div id="main_content_wrap" class="outer">
+    <section id="main_content" class="inner center">
+
+
+        <h3 class="center">QR code Reader Demo</h3>
+
+        <div  class="center" id="reader" style="width:300px;height:250px;"></div>
+
+        <h6 class="center">Result</h6>
+        <span id="read" class="center"></span>
+        <br>
+
+        <h6 class="center">Read Error (Debug only)</h6>
+        <span class="center">Will constantly show a message, can be ignored</span>
+        <span id="read_error" class="center"></span>
+
+        <br>
+        <h6 class="center">Video Error</h6>
+        <span id="vid_error" class="center"></span>
+
+        <br>
+        <br>
+        <br>
+
+        </div>
+    </section>
+    <!-- FOOTER  -->
+    <div id="footer_wrap" class="outer">
+        <footer class="inner">
+
+        </footer>
+    </div>
