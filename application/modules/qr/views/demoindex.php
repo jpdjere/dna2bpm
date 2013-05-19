4f26bdd93060ce464554390f89a9f77404779337
juanb <juanb@lmde>
2013-05-19 18:59:21 -0300

    warning message added 4 qr/cache and qr.log Signed-off-by:juanb <juanb@lmde>

--- application/modules/qr/views/demoindex.php
+++ application/modules/qr/views/demoindex.php
@@ -1,7 +1,14 @@
 <!-- HEADER -->
 <div id="header_wrap" class="text-center">
     <header class="inner">
+
         <h1 id="project_title">{title}</h1>
+    </header>
+</div>
+
+<!-- MAIN CONTENT -->
+<div id="main_content_wrap" class="text-center">
+    <section id="main_content">
         <ul class="nav nav-pills nav-stacked">
             <li>
 
@@ -16,16 +23,14 @@
                 </a>
             </li>
         </ul>
-    </header>
+        <br/>
+        <br/>
+        <div class="alert span4" >
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Warning!</strong> Make sure qr/cache and qr/log has read/write permissions
+        </div>
+    </section>
 </div>
-
-<!-- FOOTER  -->
-<div id="footer_wrap" class="outer">
-    <footer class="inner">
-
-    </footer>
-</div>
-
 <!-- FOOTER  -->
 <div id="footer_wrap" class="outer">
     <footer class="inner">
