ab287ff1b85085f622ccf97a6964fb7fface772f
astrid <astrid@lara>
2013-05-23 16:14:51 -0300

    tomamos el valor del escaneo del qrcode y lo ponemos para enviarlo por post

--- application/modules/qr/assets/jscript/qr.js
+++ application/modules/qr/assets/jscript/qr.js
@@ -2,11 +2,19 @@ $(document).ready(function(){
     $('#reader').html5_qrcode(
         function(data){
             $('#read').html(data).addClass('alert alert-success');
+            escaneo = $('#readHidden').val(data);
             color=$('body').css('background-color');
-            $('body').animate({backgroundColor: '#FFF'},200).animate({backgroundColor: color},100)
+            $('body').animate({backgroundColor: '#FFF'},200).animate({backgroundColor: color},100);
+            
             url=globals.redir;
-            window.location(url);
+            
+            $.post(url,{'redir': escaneo},function(res){});
+            
+            
             //$.ajax({type:'post'})
+         
+            
+            
         },
         function(error){
             $('#read_error').html(error);
