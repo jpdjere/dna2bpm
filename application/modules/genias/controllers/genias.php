9434c804db1d3898a2da26f51d8807def3582cf8
Juan Ignacio Borda <juanignacioborda@gmail.com>
2013-06-03 18:56:18 -0300

    test para offline json

--- application/modules/genias/controllers/genias.php
+++ application/modules/genias/controllers/genias.php
@@ -250,6 +250,32 @@ class Genias extends MX_Controller {
         $this->ui->makeui('ext.ui.php', $cpData);
     }
 
+    function Teststore() {
+        //echo $this->idu;   
+        //---Libraries
+        $this->load->library('parser');
+        $this->load->library('ui');
+
+        $cpData = $this->lang->language;
+        $segments = $this->uri->segment_array();
+        $cpData['theme'] = $this->config->item('theme');
+        $cpData['base_url'] = $this->base_url;
+        $cpData['module_url'] = $this->module_url;
+        $cpData['title'] = 'Test ofline store';
+
+
+        $cpData['js'] = array(
+            $this->module_url . 'assets/jscript/store-test/ext.data.js' => 'Base Data',
+            $this->module_url . 'assets/jscript/store-test/start.js' => 'Start Test',
+        );
+
+        $cpData['global_js'] = array(
+            'base_url' => $this->base_url,
+            'module_url' => $this->module_url,
+        );
+        $this->ui->makeui('ext.ui.php', $cpData);
+    }
+
     function Tablet() {
         //echo $this->idu;   
         //---Libraries
