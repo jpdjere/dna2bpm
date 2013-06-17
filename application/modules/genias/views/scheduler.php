9ba5f4b72bd9fa39abf84ff07778ebafbb74bfa9
gabriel <gabriel@gabriel-VGN-N350FE>
2013-06-17 18:16:48 -0300

    Genias - correciones varias tasks

--- application/modules/genias/views/scheduler.php
+++ application/modules/genias/views/scheduler.php
@@ -28,11 +28,11 @@
         <div class="row-fluid">
             
             <div class="span6">
-                    <div class="input-prepend input-append">
-                     <span class="add-on "><i class="icon-calendar"></i></span>
-                        <input  type="text"  name="dia" class="datepicker input-small" placeholder=".span6">
-                        <span class="add-on "><i class="icon-time"></i></span>
-                    </div>
+            <div class="input-prepend input-append">
+             <span class="add-on "><i class="icon-calendar"></i></span>
+                <input  type="text"  name="dia" class="datepicker input-small" placeholder="Fecha">
+                <span class="add-on "><i class="icon-time"></i></span>
+            </div>
             </div>
             <div class="span3">
             <select  name="hora" class="input-block-level">
@@ -79,68 +79,6 @@
             </select>
             </div>
         </div>
-
-
-<!--            
-           <div class="input-prepend span4">
-            <span class="add-on "><i class="icon-calendar"></i></span>
-               <input  type="text"  name="dia" class="datepicker" placeholder=".span6">
-           </div>
-            <select  name="hora" class="span2">
-            <option>01</option>
-            <option>02</option>
-            <option>03</option>
-            <option>04</option>
-            <option>05</option>
-            <option>06</option>
-            <option>07</option>
-            <option>08</option>
-            <option>09</option>
-            <option>10</option>
-            <option>11</option>
-            <option selected="selected">12</option>
-            <option>13</option>
-            <option>14</option>
-            <option>15</option>
-            <option>16</option>
-            <option>17</option>
-            <option>18</option>
-            <option>19</option>
-            <option>20</option>
-            <option>21</option>
-            <option>22</option>
-            <option>23</option>
-            <option>24</option>
-            </select>
-            <select  name="minutos" class="span2" >
-            <option selected="selected" >00</option>
-            <option>05</option>
-            <option>10</option>
-            <option>15</option>
-            <option>20</option>
-            <option>25</option>
-            <option>30</option>
-            <option>35</option>
-            <option>40</option>
-            <option>45</option>
-            <option>50</option>
-            <option>55</option>
-            </select>-->
-         
-        
-        
-
-
-
-
-         <!-- Fecha -->
-
-
-         
-         
-    
-
-    
 
     
     <textarea type="text" name="detail" placeholder="Detail" class="input-block-level" ></textarea>
