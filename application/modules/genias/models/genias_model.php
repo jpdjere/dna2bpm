7dfe83309beef9dd98fbced263f034cec14e85c9
Juan Borda <juanb@mp.gba.gov.ar>
2013-06-18 12:49:52 -0300

    Agrego get_empresas me devuelve un array

--- application/modules/genias/models/genias_model.php
+++ application/modules/genias/models/genias_model.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @class genia
  *
@@ -6,85 +7,96 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-
 class Genias_model extends CI_Model {
 
-    function __construct()
-    {
+    function __construct() {
         // Call the Model constructor
         parent::__construct();
     }
-    
+
     /* ---- TASKS---- */
-    
-    function remove_task($id){;
-        $container='container.genias_tasks';
-        $query=array('id'=>(integer)$id);
-        $rs= $this->mongo->db->$container->remove($query);
-        return $rs['err'];          
-    }
-    
-    function add_task($task){
-        $options = array('upsert' => true, 'safe' => true);
-        $container='container.genias_tasks';
-        $query=array('id'=>(integer)$task['id']);
-        $rs= $this->mongo->db->$container->update($query, $task, $options);
-        return $rs['err'];            
-    }
-    
-    function get_tasks($idu,$proyecto){
 
-        $query = array('idu' =>(double) $idu,'proyecto'=>$proyecto); 
-        $container='container.genias_tasks';
-        $result = $this->mongo->db->$container->find($query)->sort(array('id'=>-1));  
-
-        //var_dump($result, json_encode($result), $result->count());
-        return $result;
-
-    }
-    
-     /* ---- GOALS---- */
-    
-    function add_goal($goal){
-        $options = array('upsert' => true, 'safe' => true);
-        $container='container.genias_goals';
-        $query=array('id'=>(integer)$goal['id']);
-        return $this->mongo->db->$container->update($query,$goal, $options);
-    }
-    
-    function get_goals($idu){
-        $query = array('idu' =>(double) $idu);
-        $container='container.genias_goals';
-        $result = $this->mongo->db->$container->find($query)->sort(array('desde'=>-1));        
-        //var_dump($result, json_encode($result), $result->count());
-
-        return $result;
-    }
-    
-    function get_case($case){
-        $query = array('id' =>$case);
-        $container='case';
-        $result = $this->mongo->db->$container->findOne($query); 
-        return $result;
-    }
-    
-
-    // -- Config -- //
-    
-    function get_config_item($name){
-        $container='container.genias_config';
-        $query=array('name'=>$name);
-        $result = $this->mongo->db->$container->findOne($query); 
-        return $result;
-    }
-    
-        function config_set($data){
-        $container='container.genias_config';
-        $options = array('upsert' => true, 'safe' => true);
-        $query=array('name'=>'projects');
-        $rs= $this->mongo->db->$container->update($query, $data, $options);
+    function remove_task($id) {
+;
+        $container = 'container.genias_tasks';
+        $query = array('id' => (integer) $id);
+        $rs = $this->mongo->db->$container->remove($query);
         return $rs['err'];
     }
-    
+
+    function add_task($task) {
+        $options = array('upsert' => true, 'safe' => true);
+        $container = 'container.genias_tasks';
+        $query = array('id' => (integer) $task['id']);
+        $rs = $this->mongo->db->$container->update($query, $task, $options);
+        return $rs['err'];
+    }
+
+    function get_tasks($idu, $proyecto) {
+
+        $query = array('idu' => (double) $idu, 'proyecto' => $proyecto);
+        $container = 'container.genias_tasks';
+        $result = $this->mongo->db->$container->find($query)->sort(array('id' => -1));
+
+        //var_dump($result, json_encode($result), $result->count());
+        return $result;
+    }
+
+    /* ---- GOALS---- */
+
+    function add_goal($goal) {
+        $options = array('upsert' => true, 'safe' => true);
+        $container = 'container.genias_goals';
+        $query = array('id' => (integer) $goal['id']);
+        return $this->mongo->db->$container->update($query, $goal, $options);
+    }
+
+    function get_goals($idu) {
+        $query = array('idu' => (double) $idu);
+        $container = 'container.genias_goals';
+        $result = $this->mongo->db->$container->find($query)->sort(array('desde' => -1));
+        //var_dump($result, json_encode($result), $result->count());
+
+        return $result;
+    }
+
+    function get_case($case) {
+        $query = array('id' => $case);
+        $container = 'case';
+        $result = $this->mongo->db->$container->findOne($query);
+        return $result;
+    }
+
+    // -- Config -- //
+
+    function get_config_item($name) {
+        $container = 'container.genias_config';
+        $query = array('name' => $name);
+        $result = $this->mongo->db->$container->findOne($query);
+        return $result;
+    }
+
+    function config_set($data) {
+        $container = 'container.genias_config';
+        $options = array('upsert' => true, 'safe' => true);
+        $query = array('name' => 'projects');
+        $rs = $this->mongo->db->$container->update($query, $data, $options);
+        return $rs['err'];
+    }
+
+    function get_empresas($prov) {
+        $rtn=array();
+        $query = array(
+            '4651' => $prov,
+            'status' => 'activa',
+            );
+        $fields=array('id','1693','1695');
+        $container = 'container.empresas';
+        $result = $this->mongo->db->$container->find($query,$fields);
+        foreach($result as $empresa){
+            $rtn[]=$empresa;
+        }
+        return $rtn;
+    }
 
 }
