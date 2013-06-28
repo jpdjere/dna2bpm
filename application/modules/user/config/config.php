bf4b34632b7fddd263b6fd9e025f90570b097688
juanb <juanb@lmde>
2013-06-28 19:16:56 -0300

    lo dejo como estaba Signed-off-by:juanb <juanb@lmde>

--- application/modules/user/config/config.php
+++ application/modules/user/config/config.php
@@ -2,4 +2,4 @@
 $config['show_warn']	= true;
 //---run mode development /  production
 $config['run_mode']	= 'development';
-$config['default_controller']	= 'dna2';
+$config['default_controller']	= 'dna2/dashboard';
