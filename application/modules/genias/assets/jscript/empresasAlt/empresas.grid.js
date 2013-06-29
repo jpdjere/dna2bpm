ed9e415599f09692035684d1c6d5f2a4de99f12b
ichmephysto <ichmephysto@ichmephysto>
2013-06-28 22:43:33 -0300

    Genias "Valida CUIT"

--- application/modules/genias/assets/jscript/empresasAlt/empresas.grid.js
+++ application/modules/genias/assets/jscript/empresasAlt/empresas.grid.js
@@ -1,3 +1,36 @@
+/**
+ * Function que valida el algoritmo del Nro de CUIT
+ * 
+ * @param cuit
+ * @cat validate
+ * @type Vtype
+ * @author Diego
+ * 
+ **/
+Ext.apply(Ext.form.field.VTypes, {
+    CUIT: function(cuit) {
+        if (typeof (cuit) == 'undefined')
+            return true;
+        cuit = cuit.toString().replace(/[-_]/g, "");
+        if (cuit == '')
+            return true; //No estamos validando si el campo esta vacio, eso queda para el "required"
+        if (cuit.length != 11)
+            return false;
+        else {
+            var mult = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+            var total = 0;
+            for (var i = 0; i < mult.length; i++) {
+                total += parseInt(cuit[i]) * mult[i];
+            }
+            var mod = total % 11;
+            var digito = mod == 0 ? 0 : mod == 1 ? 9 : 11 - mod;
+        }
+        return digito == parseInt(cuit[10]);
+    },
+    CUITText: 'Ingrese un Nro de C.U.I.T. V&aacute;lido',
+    CUITMask: /[\d\.]/i
+});
+
 function gridClick (view,record,item,index,e,options ){
     cuit=record.data['1695'];
     EmpresaForm.loadRecord(record);    
