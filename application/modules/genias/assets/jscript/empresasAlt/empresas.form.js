b8807d9113c1c45f767753f3b2771ca7d34dfcc6
ichmephysto <ichmephysto@ichmephysto>
2013-06-29 21:35:27 -0300

    Genias "Visitas"

--- application/modules/genias/assets/jscript/empresasAlt/empresas.form.js
+++ application/modules/genias/assets/jscript/empresasAlt/empresas.form.js
@@ -35,6 +35,9 @@ var btnNew = Ext.create('Ext.Action', {
     text: '<i class="icon icon-plus"></i> Agregar',
     handler: function() {
         EmpresaForm.loadRecord(Ext.create('EmpresaModel', {}));
+        /*Reseteo si hubiera una tarea asociada anterio*/
+        Ext.getCmp('task').setValue("");
+
     }
 
 });
@@ -130,9 +133,15 @@ var EmpresaForm = Ext.create('Ext.form.Panel', {
                             record = EmpresaStore.getAt(index);
                             if (record != actualRecord) {
                                 EmpresaForm.loadRecord(record);
-                            } 
+                            }
                             EmpresaForm.setLoading(false);
-                            
+
+                            /* Para tareas relacionadas via Agenda*/
+                            var getParams = document.URL.split("/");
+                            var params = (getParams[getParams.length - 1]);
+                            Ext.getCmp('task').setValue(params);
+
+
                         } else {
                             EmpresaForm.setLoading(false);
                         }
