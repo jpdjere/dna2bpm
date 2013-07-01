6a1c0f8f05f43a3edfa2d2adb8e66cb061a2f1ba
Juan Ignacio Borda <juanignacioborda@gmail.com>
2013-07-01 15:43:58 -0300

    resumen

--- application/modules/genias/views/dashboard.php
+++ application/modules/genias/views/dashboard.php
@@ -1,116 +1,121 @@
 <!-- / Contenido -->
 <div class="container" > 
-    
-    
-<div class="row-fluid">
-    
- <!-- xxxxxxxxxxxxxxxx CREAR META  xxxxxxxxxxxxxxxx -->
 
- 
-<button type="button" class="btn " data-toggle="collapse" data-target="#meta_div">
-<i class="icon-plus"></i> Nueva meta
-</button>
-    <br/>
-    <br/>
- <div id="meta_div" class="collapse out no-transition">
-     
-<form id="form_goals" method="post" class="well">
-    <div  class="row-fluid">
-        <div class="span6">
-            <label>Proyecto</label>
-                <select name="proyecto" class="input-block-level">
-                {projects}
-                <option value="{id}">{name}</option>
-                {/projects}
-                </select>
-                <label>Genia</label>
-                <select name="genia" class="input-block-level">
-                {genias}
-                <option value="{nombre}">{nombre}</option>
-                {/genias}
-                </select>
-        <div class="">
-            <label>Cantidad</label>
-            <input type="number" name="cantidad" placeholder="Cantidad"   class="input-block-level"/>
-        </div>
-        </div>
-        <div class="span6">
-        <div class="">
-            <label>Período</label>
-            
-<!--<div class="input-append">
-<input type="text" name="desde" placeholder="Período"   class="input-block-level "/>
-</div>
-            -->
-        <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date">
-            <input type="text" name="desde" readonly="" value=""  class="input-block-level">
-            <span class="add-on"><i class="icon-calendar"></i></span>
-        </div>
-        </div>
 
-        <label>Observaciones</label>
-        <textarea name="observaciones" placeholder="Observaciones"  class="input-block-level" ></textarea>
+    <div class="row-fluid">
 
-        </div>
-    </div>
-<div  class="row-fluid">
-    <div class="span12">
+        <!-- xxxxxxxxxxxxxxxx CREAR META  xxxxxxxxxxxxxxxx -->
 
-        <button class="btn btn-block btn-primary " type="submit" id="bt_save"><i class="icon-save"></i>  Agregar</button>  
-    </div> 
-            
-</div> 
 
-</form>  
+        <button type="button" class="btn " data-toggle="collapse" data-target="#meta_div">
+            <i class="icon-plus"></i> Nueva meta
+        </button>
+        <br/>
+        <br/>
+        <div id="meta_div" class="collapse out no-transition">
 
-</div>
+            <form id="form_goals" method="post" class="well">
+                <div  class="row-fluid">
+                    <div class="span6">
+                        <label>Proyecto</label>
+                        <select name="proyecto" class="input-block-level">
+                            {projects}
+                            <option value="{id}">{name}</option>
+                            {/projects}
+                        </select>
+                        <label>Genia</label>
+                        <select name="genia" class="input-block-level">
+                            {genias}
+                            <option value="{nombre}">{nombre}</option>
+                            {/genias}
+                        </select>
+                        <div class="">
+                            <label>Cantidad</label>
+                            <input type="number" name="cantidad" placeholder="Cantidad"   class="input-block-level"/>
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <div class="">
+                            <label>Período</label>
 
-    <br/>
-    <br/>
+                            <!--<div class="input-append">
+                            <input type="text" name="desde" placeholder="Período"   class="input-block-level "/>
+                            </div>
+                            -->
+                            <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date">
+                                <input type="text" name="desde" readonly="" value=""  class="input-block-level">
+                                <span class="add-on"><i class="icon-calendar"></i></span>
+                            </div>
+                        </div>
 
- 
- 
-</div> 
-<!-- xxxxxxxxxxxxxxxx METAS  xxxxxxxxxxxxxxxx -->
-<?php
-foreach($metas as $meta){
-?>
- <div  class="row-fluid " >
-       <div class="span12 <?php echo $meta['status_class'];?>">  
-        <div  class="row-fluid" >
-            <div class="span12 ">  
-                <h3><?php echo $meta['proyecto_nombre'];?><span class="pull-right"><?php echo $meta['cumplidas_count'];?>/<?php echo $meta['cantidad'];?></span></h3>
-            </div>
-        </div>
-        <div  class="row-fluid" >
-            <div class="span4"> 
-                <ul class="unstyled">
-                <li><i class="icon-calendar" ></i> Período: <?php echo $meta['desde'];?></li>
-                <li><i class="icon-eye-open" ></i> Estado: <span class="label <?php echo $meta['label_class'];?>"><i class="<?php echo $meta['status_icon_class'];?>"></i>&nbsp;<?php echo $meta['status'];?></span></li>
-                <li><i class="icon-user" ></i> Autor: <?php echo $meta['owner'];?></li>
-                <li><i class="icon-flag" ></i> Genia: <?php echo $meta['genia'];?></li>
-                </ul>
-            </div>
-            <div class="span8"> 
-                <div class="observaciones" >
-                <?php echo $meta['observaciones'];?>
+                        <label>Observaciones</label>
+                        <textarea name="observaciones" placeholder="Observaciones"  class="input-block-level" ></textarea>
+
+                    </div>
                 </div>
-                
-               <?php 
-               if($rol=='coordinador'&& $meta['status_class']=='well status_open'){
-                    echo '<a class="btn btn-primary pull-right" href="'.$meta['url_case'].'" targe="_blank"  type="button" style="margin-top:12px;t"><i class="icon-thumbs-up-alt"></i> Aprobar</a>';
-               }
-               ?>
-                
-            </div>
-        </div> 
-     </div>
- </div>
-<?php
-}//each
-?>
-<!-- ============= metas  ============= -->
+                <div  class="row-fluid">
+                    <div class="span12">
 
- 
+                        <button class="btn btn-block btn-primary " type="submit" id="bt_save"><i class="icon-save"></i>  Agregar</button>  
+                    </div> 
+
+                </div> 
+
+            </form>  
+
+        </div>
+
+        <br/>
+        <br/>
+
+        <!-- Resumen --> 
+        {if {rol}=='coordinador'}
+        <div class="alert {resumen_class}">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Sus genias tienen {goal_cumplidas} de {goal_cantidad} objetivos cumplidos.</strong> 
+        </div>
+        {/if}
+    </div> 
+    <!-- xxxxxxxxxxxxxxxx METAS  xxxxxxxxxxxxxxxx -->
+    <?php
+    foreach ($metas as $meta) {
+        ?>
+        <div  class="row-fluid " >
+            <div class="span12 <?php echo $meta['status_class']; ?>">  
+                <div  class="row-fluid" >
+                    <div class="span12 ">  
+                        <h3><?php echo $meta['proyecto_nombre']; ?><span class="pull-right"><?php echo $meta['cumplidas_count']; ?>/<?php echo $meta['cantidad']; ?></span></h3>
+                    </div>
+                </div>
+                <div  class="row-fluid" >
+                    <div class="span4"> 
+                        <ul class="unstyled">
+                            <li><i class="icon-calendar" ></i> Período: <?php echo $meta['desde']; ?></li>
+                            <li><i class="icon-eye-open" ></i> Estado: <span class="label <?php echo $meta['label_class']; ?>"><i class="<?php echo $meta['status_icon_class']; ?>"></i>&nbsp;<?php echo $meta['status']; ?></span></li>
+                            <li><i class="icon-user" ></i> Autor: <?php echo $meta['owner']; ?></li>
+                            <li><i class="icon-flag" ></i> Genia: <?php echo $meta['genia']; ?></li>
+                        </ul>
+                    </div>
+                    <div class="span8"> 
+                        <div class="observaciones" >
+                            <?php echo $meta['observaciones']; ?>
+                        </div>
+
+                        <?php
+                        if ($rol == 'coordinador' && $meta['status_class'] == 'well status_open') {
+                            echo '<a class="btn btn-primary pull-right" href="' . $meta['url_case'] . '" targe="_blank"  type="button" style="margin-top:12px;t"><i class="icon-thumbs-up-alt"></i> Aprobar</a>';
+                        }
+                        ?>
+
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <?php
+    }//each
+    ?>
+    <!-- ============= metas  ============= -->
+
+
 </div> 
 
