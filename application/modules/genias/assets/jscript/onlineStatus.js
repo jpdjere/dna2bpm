d4f46569bcf2a13b4f70da06ee58ffcc3a1fce0d
ichmephysto <ichmephysto@ichmephysto>
2013-07-02 11:19:10 -0300

    Genias "Implementar addEventListener Online/Offline Support #3193"

--- application/modules/genias/assets/jscript/onlineStatus.js
+++ application/modules/genias/assets/jscript/onlineStatus.js
@@ -1,21 +1,30 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Function EventListener for Online/Offline Status
+ * 
  */
-function updateOnlineStatus(msg) {   
+function updateOnlineStatus(msg) {
     var status = document.getElementById("status");
-    var condition = navigator.onLine ? "ONLINE" : "OFFLINE";    
-    console.log("Event: " + msg + "; status...=" + condition + "\n");
-    Ext.getCmp('escenario').setTitle('Escenario Pyme' . condition);
-    //status.getCmp = "Escenario Pyme" . condition;
+    var condition = navigator.onLine ? "On-Line" : "Off-Line";
+    
+    if(navigator.onLine){
+        Ext.getCmp('btnSync').show();
+    } else {
+        Ext.getCmp('btnSync').hide();
+    }
+    
+    var state = Ext.getElementById("status");
+    state.innerHTML = '<i class="icon icon-circle"></i> '+condition; 
+    
 }
 
 function loaded() {
     updateOnlineStatus("load");
-    document.body.addEventListener("offline", function() {updateOnlineStatus("offline")}, false);
-    document.body.addEventListener("online", function() {updateOnlineStatus("online")}, false);
+    document.body.addEventListener("offline", function() {
+        updateOnlineStatus("offline")
+    }, false);
+    document.body.addEventListener("online", function() {
+        updateOnlineStatus("online")
+    }, false);
 }
-
-
 
 
