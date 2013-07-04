a227671291ca3ba7c1df94ad24720f0c54dd5783
juanb <juanb@lmde>
2013-07-04 19:21:51 -0300

    Layout 3 Signed-off-by:juanb <juanb@lmde>

--- application/modules/inventory/views/avery6.php
+++ application/modules/inventory/views/avery6.php
@@ -1,19 +1,21 @@
 <div  class="row">
     {qr}
     <div class="span6">
-        <div class="span6">
-            <div class="img-polaroid">
-                <img src="{src}"/>
+        <div class="row">
+            <div class="span6">
+                <div class="img-polaroid">
+                    <img src="{src}"/>
+                </div>
             </div>
-        </div>
-        <div class="span6">
+            <div class="span6">
 
-            <h3>
-                {PDE}
-            </h3>
-            {nombre_empresa}
-            <br/>
-            {cuit_empresa}
+                <h3>
+                    {PDE}
+                </h3>
+                {nombre_empresa}
+                <br/>
+                {cuit_empresa}
+            </div>
         </div>
     </div>
     {/qr}
