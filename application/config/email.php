74e7c259bf43b0375e4a6376202dd58b358b6bec
astrid <astrid@lara>
2013-07-10 16:40:08 -0300

    test 3 de envio de mail para enviar el token de recuperacion de password

--- application/config/email.php
+++ application/config/email.php
@@ -1,8 +1,8 @@
 <?php
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'www.dna2.org';
-$config['smtp_user'] = 'dna2';
-$config['smtp_pass'] = 'novanet';
+$config['smtp_host'] = '168.101.16.10';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
 $config['smtp_timeout'] = '30';
 $config['protocol'] = 'smtp';
 $config['charset'] = 'UTF-8';
@@ -11,5 +11,4 @@ $config['wordwrap'] = TRUE;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 ?>
