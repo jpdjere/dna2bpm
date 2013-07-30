ffcdd31e33b99361295d897f89feb41dc90f333a
gabriel <gabriel@gabriel-VGN-N350FE>
2013-07-30 12:42:02 -0300

    Credito fiscal arreglos envio

--- application/modules/genias/controllers/genias.php
+++ application/modules/genias/controllers/genias.php
@@ -890,7 +890,8 @@ class Genias extends MX_Controller {
                 $thisEmpresa['partido_txt'] = '<span class="label label-important"><i class="icon-info-sign"/> COMPLETAR! </span>';
             }
         }
-
+var_dump($empresas);
+exit();
         $rtnArr = array();
         $rtnArr['totalCount'] = count($empresas);
         $rtnArr['rows'] = $empresas;
