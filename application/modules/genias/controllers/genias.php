2bb5a1932d6cd3858beab3a0daced58181c529fd
gabriel <gabriel@gabriel-VGN-N350FE>
2013-08-01 13:41:27 -0300

    Dashboard Genias - Arreglo de los contadores de las pestañas de genias que no estaban contabilizando bien. Al presionar una pestaña de una genia determinada, ahora ademas de mostrar el resumen filtra las metas para mostrar solo las de esa pestaña.

--- application/modules/genias/controllers/genias.php
+++ application/modules/genias/controllers/genias.php
@@ -56,8 +56,8 @@ class Genias extends MX_Controller {
         foreach($genias['genias'] as $mygenia){
            $customData['goal_cantidad'][(string)$mygenia['_id']]=0;
            $customData['goal_cumplidas'][(string)$mygenia['_id']]=0;
-            if($this->idu==-1108639299){//
-            var_dump($customData);
+            if($this->idu==-1108639299){//ssss
+                //sasss
             echo $mygenia['_id'];
             echo "<br>";
             }
