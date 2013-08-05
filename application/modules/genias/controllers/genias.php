3533cb35229addd3a657a88eb13136b91d4bd2bb
gabriel <gabriel@gabriel-VGN-N350FE>
2013-08-05 11:46:09 -0300

    Test dashboard

--- application/modules/genias/controllers/genias.php
+++ application/modules/genias/controllers/genias.php
@@ -58,13 +58,13 @@ class Genias extends MX_Controller {
            $customData['goal_cumplidas'][(string)$mygenia['_id']]=0;
         }
 
-            if($this->idu==-1108639299){//
-            var_dump($customData['goal_cumplidas']);
-           // exit();
-            }    
+   
         
         $goals = $this->genias_model->get_goals((int) $this->idu);
-
+            if($this->idu==-1108639299){//
+            var_dump($goals);
+           // exit();
+            } 
         foreach ($goals as $goal) {
             
             // === Nombre del proyecto y select de proyectos para las metas
