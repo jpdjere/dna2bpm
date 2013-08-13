3e87dd4e72bea02b49fac8c496b4e6cdc02032ee
juanb <juanb@lmde>
2013-08-13 17:47:04 -0300

    better appcaché Signed-off-by:juanb <juanb@lmde>

--- application/modules/genias/controllers/genias.php
+++ application/modules/genias/controllers/genias.php
@@ -533,6 +533,8 @@ class Genias extends MX_Controller {
         $cpData['js'] = array(
             $this->module_url . 'assets/jscript/onlineStatus.js' => 'Online/Offline Status',
             $this->base_url . "jscript/ext/src/ux/form/SearchField.js" => 'Search Field',
+            $this->base_url . "jscript/ext/src/ux/statusbar/StatusBar.js" =>'Status Bar',
+            $this->base_url . "jscript/ext/src/ux/LiveFilterGridPanel.js" =>'Live Filter Panel',
             //$this->module_url . 'assets/jscript/ext.settings.js' => 'Ext Settings',
             $this->module_url . 'assets/jscript/empresas.ext.data.js' => 'Base Data',
             $this->module_url . 'assets/jscript/empresasAlt/btnSync.js' => 'btnSync',
