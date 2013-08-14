043c4bd1e651bdb91448bcbdacd27f441f3b6b24
gabriel <gabriel@gabriel-VGN-N350FE>
2013-08-14 18:34:40 -0300

    Testeo problema con matilde

--- application/modules/genias/controllers/genias.php
+++ application/modules/genias/controllers/genias.php
@@ -138,7 +138,7 @@ class Genias extends MX_Controller {
 
            if($this->idu==150787571){//
           // $mygoals=array();
-               $mygoals = array_slice($mygoals, 5);
+               $mygoals = array_slice($mygoals, 0, 5);
           // var_dump($mygoals);
            } 
 
