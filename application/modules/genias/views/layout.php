27b05d10bbef4b5a8274d06e40ab6938f6143ff4
gabriel <gabriel@gabriel-VGN-N350FE>
2013-08-21 12:10:15 -0300

    Layout , corrijo una verison subida x error sin manifiesto

--- application/modules/genias/views/layout.php
+++ application/modules/genias/views/layout.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<!--<html lang="es" manifest="{base_url}genias/manifest/offline.appcache">-->
-<html lang="es" >
+<html lang="es" manifest="{base_url}genias/manifest/offline.appcache">
+
     <head>
         <title>DNA&sup2; Admin</title>
         <meta charset="UTF-8" />
