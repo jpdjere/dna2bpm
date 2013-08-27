697ab358e945def14f78bbac656ac03595d6c528
ich <ich@ich>
2013-08-27 12:25:24 -0300

    Support #3510 Modificacion o eliminacion datos seguimiento

--- application/modules/genias/controllers/visitas_remote.php
+++ application/modules/genias/controllers/visitas_remote.php
@@ -14,42 +14,45 @@ class Visitas_remote extends MX_Controller {
         $this->user->authorize('modules/genias/controllers/genias');
         //----LOAD LANGUAGE
         $this->lang->load('library', $this->config->item('language'));
-        $this->idu = (int) $this->session->userdata('iduser');        
+        $this->idu = (int) $this->session->userdata('iduser');
         $this->containerGenias = 'container.genias_visitas';
     }
 
     /* GENIAS */
+
     public function Insert() {
         $container = $this->containerGenias;
+        
 
         $input = json_decode(file_get_contents('php://input'));
 
         foreach ($input as $thisform) {
-            $form= get_object_vars($thisform);            
-            
+            $form = get_object_vars($thisform);
+
             /* GENERO ID */
-            $id =  $this->app->genid($container);
+            $id = $this->app->genid($container);
             unset($form['id']);
-            $form=array_filter($form);
-           
-            $form = (array) $form;            
-                $form['idu'] = (int) ($this->idu);
+            $form = array_filter($form);
+
+            $form = (array) $form;
+            $form['idu'] = (int) ($this->idu);
 
             /* Insert/Update dato */
             $result = $this->app->put_array($id, $container, $form);
 
-             if ($result) {               
+            if ($result) {
                 /* Update Goal */
-                $this->genias_model->goal_update('2',$id);
+                $this->genias_model->goal_update('2', $id);
                 $out = array('status' => 'ok');
             } else {
                 $out = array('status' => 'error');
             }
-            
         }
-           $this->genias_model->touch($form['cuit']);     
+        
+            $this->genias_model->touch($form['cuit']);
+        
     }
-  
+
     /*
      * VIEW
      */
@@ -57,7 +60,7 @@ class Visitas_remote extends MX_Controller {
     public function View() {
 
         $container = $this->containerGenias;
-        $query = array('7406' => (int)($this->idu));
+        $query = array('7406' => (int) ($this->idu));
         $resultData = $this->mongo->db->$container->find($query);
 
         foreach ($resultData as $returnData) {
@@ -73,26 +76,22 @@ class Visitas_remote extends MX_Controller {
             ));
         }
     }
-    
-    
-    public function Remove() {
-        echo "destroy";
-        exit();
+
+    public function Remove() {       
         $container = $this->containerGenias;
-        $query = array('_id' => (int)($this->idu));
-        $resultData = $this->mongo->db->$container->find($query);
-
-        foreach ($resultData as $returnData) {
-            $fileArrMongo[] = $returnData;
-        }
-        //return $fileArrMongo;             
-
-        if (!empty($fileArrMongo)) {
-            echo json_encode(array(
-                'success' => true,
-                'message' => "Loaded data",
-                'data' => $fileArrMongo
-            ));
-        }
+        $input = json_decode(file_get_contents('php://input'));
+         foreach ($input as $thisform) {
+            $form = get_object_vars($thisform);              
+            $resultData = $this->genias_model->visitas_remove($container, $form['id']);
+             if ($resultData) {
+                /* Update Goal */
+                $this->genias_model->goal_remove($form['id']);
+                $out = array('status' => 'ok');
+            } else {
+                $out = array('status' => 'error');
+            }
+         }
+       
     }
+
 }
