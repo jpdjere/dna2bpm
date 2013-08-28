5dca6023a96a4d4693a9c434b02027b8b48478c0
Juan Ignacio Borda <juanignacioborda@gmail.com>
2013-08-28 14:53:59 -0300

    agrego idu al manifiesto (como md5)

--- application/modules/genias/controllers/manifest.php
+++ application/modules/genias/controllers/manifest.php
@@ -21,6 +21,8 @@ class manifest extends MX_Controller {
         $cpData['fecha'] = date('Y-m-d H');
         $cpData['base_url'] = base_url();
         $cpData['module_url'] = base_url() . 'genias/';
+        $cpData['sess'] = md5((int) $this->session->userdata('iduser'));
+        
         header("Content-Type: text/cache-manifest; charset=utf-8");
         //header('Cache-Control: no-cache,no-store');
         $content = $this->parser->parse_string($file_content, $cpData);
