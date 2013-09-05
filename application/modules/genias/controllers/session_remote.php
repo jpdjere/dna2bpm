c0ca3af3995551df09db4a73d36901918fafb3a1
ich <ich@ich>
2013-09-05 15:21:43 -0300

    Fix #3720 id usuario vacio

---
+++ application/modules/genias/controllers/session_remote.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Session_remote extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->user->authorize('modules/genias/controllers/genias');
+        $this->load->library('parser');
+        $this->load->model('user');
+        $this->load->model('app');
+        $this->load->model('genias_model');
+        $this->user->authorize('modules/genias/controllers/genias');
+        //----LOAD LANGUAGE
+        $this->lang->load('library', $this->config->item('language'));
+        $this->idu = (int) $this->session->userdata('iduser');
+    }
+
+    /*
+     * CHECK LOGIN
+     */
+
+    public function ChkLogin() {
+        $iduchK = array('idu' => $this->idu);
+        $rtnArr = array();
+        $rtnArr['totalCount'] = 1;
+        $rtnArr['rows'] = $iduchK;
+        echo json_encode($rtnArr);
+    }
+
+}
