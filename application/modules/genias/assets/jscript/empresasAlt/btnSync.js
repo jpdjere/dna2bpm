3ab305d4962242dc33196f6138bca89f00784dd7
ich <ich@ich>
2013-09-05 15:23:29 -0300

    Fix #3720 id usuario vacio

--- application/modules/genias/assets/jscript/empresasAlt/btnSync.js
+++ application/modules/genias/assets/jscript/empresasAlt/btnSync.js
@@ -5,61 +5,75 @@ var btnSync = Ext.create('Ext.Action',
             iconCls: 'icon icon-cloud-upload',
             tooltip: 'Sincronizar cambios',
             handler: function() {
+                /*Check ID user*/
+                checkStatusStore.load({
+                    scope: this,
+                    callback: function(records, operation, success) {
+                        if (success) {
+                            var records = new Array();
+                            //---me guardo el proxy offline
+                            offlineProxy = storeEmpresaOffline.proxy;
+                            //---le pongo el proxy AJAX                   
+                            //---Marcamos Dirty cada uno de los registros
 
-                var records = new Array();
-                //---me guardo el proxy offline
-                offlineProxy = storeEmpresaOffline.proxy;
-                //---le pongo el proxy AJAX                   
-                //---Marcamos Dirty cada uno de los registros
-                
-                /*Datos Empresas*/
-                storeEmpresaOffline.each(function(rec) {
-                    rec.setDirty();
-                    storeEmpresa.add(rec)
-                });
-                storeEmpresa.sync();
+                            /*Datos Empresas*/
+                            storeEmpresaOffline.each(function(rec) {
+                                rec.setDirty();
+                                storeEmpresa.add(rec)
+                            });
+                            storeEmpresa.sync();
 
-                /*Datos Visitas*/
-                storeVisitaOffline.each(function(rec) {                    
-                        rec.setDirty();
-                        storeVisita.add(rec);
-                });
-                storeVisita.sync();
-                
-                /*REMOVE Datos Visitas*/
-                 storeVisitaOfflineDelete.each(function(rec) {                      
-                     rec.setDirty();   
-                     storeVisitaDelete.add(rec);
-                });
-                storeVisitaDelete.sync();
-                
+                            /*Datos Visitas*/
+                            storeVisitaOffline.each(function(rec) {
+                                rec.setDirty();
+                                storeVisita.add(rec);
+                            });
+                            storeVisita.sync();
 
-                /*Datos Encuestas*/
-                storeEncuestasOffline.each(function(rec) {
-                    rec.setDirty();
-                    storeEncuesta.add(rec)
-                });
-                storeEncuesta.sync();
-
-                var getCount = storeEncuestasOffline.getCount() + storeVisitaOffline.getCount() + storeEmpresaOffline.getCount() + storeVisitaOfflineDelete.getCount();
+                            /*REMOVE Datos Visitas*/
+                            storeVisitaOfflineDelete.each(function(rec) {
+                                rec.setDirty();
+                                storeVisitaDelete.add(rec);
+                            });
+                            storeVisitaDelete.sync();
 
 
+                            /*Datos Encuestas*/
+                            storeEncuestasOffline.each(function(rec) {
+                                rec.setDirty();
+                                storeEncuesta.add(rec)
+                            });
+                            storeEncuesta.sync();
 
-                if (getCount != 0) {
-                    Ext.Msg.alert('Encenario Pyme', '<h5>Actualizado con Exito</h5>');
-                } else {
-                    Ext.getCmp('btnSync').setText('No Hay informacion para actualizar');
-                }
+                            var getCount = storeEncuestasOffline.getCount() + storeVisitaOffline.getCount() + storeEmpresaOffline.getCount() + storeVisitaOfflineDelete.getCount();
 
-                /*Borro la informacion local*/
-                storeEmpresaOffline.removeAll();
-                storeVisitaOffline.removeAll();
-                storeEncuestasOffline.removeAll();
-                storeVisitaOfflineDelete.removeAll();
 
-                /*Actualizo el contador*/
-                getCount = storeEncuestasOffline.getCount() + storeVisitaOffline.getCount() + storeEmpresaOffline.getCount() + storeVisitaOfflineDelete.getCount();
-                Ext.getCmp('btnSync').setText('Hay (' + getCount + ') para actualizar');
+
+                            if (getCount != 0) {
+                                Ext.Msg.alert('Encenario Pyme', '<h5>Actualizado con Exito</h5>');
+                            } else {
+                                Ext.getCmp('btnSync').setText('No Hay informacion para actualizar');
+                            }
+
+                            /*Borro la informacion local*/
+                            storeEmpresaOffline.removeAll();
+                            storeVisitaOffline.removeAll();
+                            storeEncuestasOffline.removeAll();
+                            storeVisitaOfflineDelete.removeAll();
+
+                            /*Actualizo el contador*/
+                            getCount = storeEncuestasOffline.getCount() + storeVisitaOffline.getCount() + storeEmpresaOffline.getCount() + storeVisitaOfflineDelete.getCount();
+                            Ext.getCmp('btnSync').setText('Hay (' + getCount + ') para actualizar');
+                        } else {
+                            /*
+                             * IDU check 
+                             * si no tiene idu, vuelve al login
+                             *
+                             **/
+                            window.location.replace("../genias");
+                        }
+                    }
+                })
             }
         }
 );
