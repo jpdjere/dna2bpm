e99c7bcd369b0e777261455282f2880384a8753c
gabriel <gabriel@gabriel-VGN-N350FE>
2013-09-16 17:48:14 -0300

    Seleccion de visitas por mes en el dashboard

--- application/modules/genias/models/genias_model.php
+++ application/modules/genias/models/genias_model.php
@@ -519,7 +519,7 @@ class Genias_model extends CI_Model {
 
             $email=(empty($empresa[1703]))?('-'):($empresa[1703]);
             $razon_social=(empty($empresa[1693]))?('-'):($empresa[1693]);
-            if($visita['idu']!=0 && in_array($visita['idu'],$usuarios)){
+            if($visita['idu']!=0 || in_array($visita['idu'],$usuarios)){
             $username="{$usuarios[$visita['idu']]['name']} {$usuarios[$visita['idu']]['lastname']}";
 
             }else{
