caf11edcd5076d22015b41e0b5f0b811b1e9e73f
ich <daotero@industria.gob.ar>
2013-11-07 11:15:28 -0300

    Support #4663

--- application/modules/sgr/views/layout.php
+++ application/modules/sgr/views/layout.php
@@ -29,7 +29,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="brand" href="#">Sgr</a>
+                    <a class="brand" href="#">SOCIEDADES DE GARANTIAS RECIPROCAS</a>
 
                     <div class="nav-collapse collapse">
                         <ul class="nav">
