732cbed47b8ad4d05f1ed62906692a22ef498661
Diego <daotero@industria.gob.ar>
2013-12-09 14:58:44 -0300

    Feature #3839: Formulario Instituciones

---
+++ application/modules/genias/controllers/visitas_remote_instituciones.php
@@ -0,0 +1,80 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Visitas_remote_instituciones extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->library('parser');
+        $this->load->model('user');
+        $this->load->model('app');
+        $this->load->model('genias_model');        
+        //----LOAD LANGUAGE
+        $this->lang->load('library', $this->config->item('language'));
+        $this->idu = (int) $this->session->userdata('iduser');
+        $this->containerGenias = 'container.genias_visitas';
+        if(!$this->idu){
+            header("$this->module_url/user/logout");
+            exit();
+        }
+    }
+    
+    
+    /* GENIAS INSITITUCIONES */
+    public function Insert() {     
+        
+        $container = $this->containerGenias;        
+
+        $input = json_decode(file_get_contents('php://input'));
+
+        foreach ($input as $thisform) {
+            $form = get_object_vars($thisform);
+
+            /* GENERO ID */
+            $id = $this->app->genid($container);
+            unset($form['id']);
+            $form = array_filter($form);
+
+            $form = (array) $form;
+            $form['idu'] = (int) ($this->idu);
+
+            /* Insert/Update dato */
+            $result = $this->app->put_array($id, $container, $form);
+            
+            
+            if ($result) {
+                /* Update Goal */
+                $newResult = $this->genias_model->goal_update('4', $id);
+                $out = array('status' => 'ok');
+            } else {
+                $out = array('status' => 'error');
+            }
+            var_dump($result);
+             return $out;
+        }
+        
+           
+        //$this->genias_model->touch($form['cuit']);
+        
+    }  
+
+    public function Remove() {              
+        
+        
+        $container = $this->containerGenias;
+        $input = json_decode(file_get_contents('php://input'));
+         foreach ($input as $thisform) {
+            $form = get_object_vars($thisform);              
+            $resultData = $this->genias_model->visitas_remove($container, $form['id']);
+             if ($resultData) {
+                $out = array('status' => 'ok');                
+            } else {
+                $out = array('status' => 'error');
+            }
+         }
+       
+    }
+
+}
