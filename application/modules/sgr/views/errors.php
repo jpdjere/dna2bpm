2e308bd93ee29c97ccee73918c57e6b3d95ef6b8
Vaio <Vaio@Vaio-PC>
2013-12-23 15:50:31 -0300

    Support #4663 Anexo 6 Movimiento de Capital Social

--- application/modules/sgr/views/errors.php
+++ application/modules/sgr/views/errors.php
@@ -1,12 +1,18 @@
 <div class="row-fluid test" id="barra_user" > 
     <ul class="breadcrumb" style="margin-bottom:0px;padding-bottom:0px" >
-
+        <button type="button" class="btn hide_offline" data-toggle="collapse" data-target="#file_div">
+            <i class="icon-plus"></i>  Seleccionar Archivos a Procesar
+        </button> 
+        {if sgr_period}
+        <button type="button" id="no_movement" class="no_movement" value="{sgr_period}">
+            <i class="icon-plus"></i>  Asociar el periodo {sgr_period} a "Sin Movimientos"
+        </button>
+        {/if}
         <li class="pull-right perfil">
-            <span id="status"></span>
-            <a title="{usermail}">{username}</a> <i class="icon-angle-right"></i> <i class="{rol_icono}"></i> {rol}
+            <span id="status"></span>{sgr_nombre}
+            <i class="{rol_icono}"></i>  <a   href="{base_url}user/logout"> {username}</a> [{rol}]
         </li>
     </ul>
-
 </div>
 <!-- ==== Contenido ==== -->
 <div class="container" > 
