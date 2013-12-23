cf297ced9505359fd8d69751a639c8e2cf241f6f
Vaio <Vaio@Vaio-PC>
2013-12-23 12:19:01 -0300

    Support #4663 Anexo 6 Movimiento de Capital Social

--- application/modules/sgr/assets/jscript/dashboard.js
+++ application/modules/sgr/assets/jscript/dashboard.js
@@ -48,8 +48,9 @@ $(document).ready(function() {
     $('button.no_movement').click(function() {
         var no_movement = $('#no_movement').val();
         bootbox.confirm("Are you sure ?" + no_movement, function(result) {
-            
-            alert("Confirm result: " + result);
+            if($result){
+            add_no_movement();
+            }
         });
 
 
