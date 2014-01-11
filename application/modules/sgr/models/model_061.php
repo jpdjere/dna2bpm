b1cb07e9d81bf9b5e2842b201596b5e0b9b2e601
Vaio <Vaio@Vaio-PC>
2014-01-11 16:18:53 -0300

    Support #4664 Anexo 6.1 Relaciones de Vinculación

--- application/modules/sgr/models/model_061.php
+++ application/modules/sgr/models/model_061.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Model_06 extends CI_Model {
+class Model_061 extends CI_Model {
 
     public function __construct() {
         // Call the Model constructor
