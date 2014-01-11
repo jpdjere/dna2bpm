96a9328735647331d0d4f00f55ab81389a213ea8
Vaio <Vaio@Vaio-PC>
2014-01-11 15:16:02 -0300

    Support #4664 Anexo 6.1 Relaciones de Vinculación

--- application/modules/sgr/libraries/validators/lib_061_error_legend.php
+++ application/modules/sgr/libraries/validators/lib_061_error_legend.php
@@ -17,7 +17,18 @@ class Lib_061_error_legend {
                 break;
             case "A.3": 
                 $result_error = '(Fila Nro.'. $row .') A: Todos los Socios que fueron informados como Incorporados en el Anexo 6 – Movimientos de Capital Social, deben figurar en esta columna.';
-                break;            
+                break;
+             case "B.1": 
+                $result_error = '(Fila Nro.'. $row .') B: El campo no puede estar vacío y debe contener uno de los siguientes parámetros:
+SI
+NO';
+                break;
+            case "B.2": 
+                $result_error = '(Fila Nro.'. $row .') B: Si el CUIT informado en la Columna A comienza con 30 o 33 (Correspondiente a Personas Jurídicas) la opción debe ser “SI”.';
+                break;
+            case "B.3": 
+                $result_error = '(Fila Nro.'. $row .') B: Si se indica la opción “NO” el CUIT no puede estar más de una vez en la Columna A de este Anexo,  y las Columnas C, D, E, y F deben estar vacías.';
+                break;
         }
 
         return $result_error;
