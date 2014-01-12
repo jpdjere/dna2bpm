5f164a3bb97e03c74f735a98f68ece1a9b3e1858
Vaio <Vaio@Vaio-PC>
2014-01-11 22:12:05 -0300

    Support #4664 Anexo 6.1 Relaciones de Vinculación

--- application/modules/sgr/controllers/sgr.php
+++ application/modules/sgr/controllers/sgr.php
@@ -600,18 +600,23 @@ Información correspondiente al período 11/2013 | IMPRIMIR | <a href='javascrip
             <div class="alert {resumen_class}" id="' . $i . '"><ul>';
             $processed = $this->sgr_model->get_processed($anexo, $this->sgr_id, $i);
             foreach ($processed as $file) {
-                $download = anchor('anexos_sgr/' . $file['name'], ' <i class="fa fa-download" alt="Descargar"> DESCARGAR</i>', array('class' => 'btn btn-success'));
-                $print_filename = ($file['filename'] == "SIN MOVIMIENTOS") ? $file['filename'] : substr($file['filename'], 0, -25);
-                $print_file = anchor('/sgr/print_anexo/' . $file['filename'], ' <i class="fa fa-external-link" alt="Imprimir"> IMPRIMIR</i>', array('target' => '_blank', 'class' => 'btn btn-success'));
+                
+                $print_filename = substr($file['filename'], 0, -25);
+                
+                if($file['filename'] != "SIN MOVIMIENTOS"){
+                    $disabled_link = 'disabled_link';
+                    $print_filename = $file['filename'];
+                }
+                
+                
+                $download = anchor('anexos_sgr/' . $file['name'], ' <i class="fa fa-download" alt="Descargar"> DESCARGAR</i>', array('class' => 'btn btn-success' . $disabled_link));               
+                $print_file = anchor('/sgr/print_anexo/' . $file['filename'], ' <i class="fa fa-external-link" alt="Imprimir"> IMPRIMIR</i>', array('target' => '_blank', 'class' => 'btn btn-success' . $disabled_link));
 
                 $rectifica_link_class = ($this->session->userdata['period']) ? 'rectifica-warning' : 'rectifica-link';
-                $rectify = anchor($file['period'] . "/" . $anexo, '<i class="fa fa-undo" alt="Rectificar"> RECTIFICAR</i>', array('class' => $rectifica_link_class . ' btn btn-success link_disabled' ));
-
-                if ($file['filename'] == "SIN MOVIMIENTOS") {
-                    $list_files .= '<li><i class="fa fa-download" alt="Descargar"> DESCARGAR</i> | <i class="fa fa-external-link" alt="Imprimir"> IMPRIMIR</i>  | ' . $rectify . ' | ' . $print_filename . ' [' . $file['period'] . '][' . $file['status'] . '] </li>';
-                } else {
-                    $list_files .= "<li>" . $download . " | " . $print_file . " | " . $rectify . " | " . $print_filename . "  [" . $file['period'] . "][" . $file['status'] . "] </li>";
-                }
+                $rectify = anchor($file['period'] . "/" . $anexo, '<i class="fa fa-undo" alt="Rectificar"> RECTIFICAR</i>', array('class' => $rectifica_link_class . ' btn btn-success' ));
+                
+                $list_files .= "<li>" . $download . " | " . $print_file . " | " . $rectify . " | " . $print_filename . "  [" . $file['period'] . "][" . $file['status'] . "] </li>";
+                
             }
             $list_files .= '</ul></div>
         </div>';
