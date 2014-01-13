4774ed700e095ba4fa50e85dc1b58720cc71db7f
Vaio <Vaio@Vaio-PC>
2014-01-12 22:08:17 -0300

    Support #4664 Anexo 6.1 Relaciones de Vinculación

--- application/modules/sgr/controllers/sgr.php
+++ application/modules/sgr/controllers/sgr.php
@@ -614,7 +614,7 @@ Información correspondiente al período 11/2013 | IMPRIMIR | <a href='javascrip
                 $script_href = $file['period'] . "/" . $anexo;
                 $rectifica_link_class = ($this->session->userdata['period']) ? 'rectifica-warning_' . $file['period']  : 'rectifica-link_' . $file['period'];
                 //$rectify = anchor('', '<i class="fa fa-undo" alt="Rectificar"> RECTIFICAR</i>', array('id'=> $rectifica_link_class, 'class' =>  ' btn btn-danger', 'onclick'=>'rectify('.$script_href.');'));
-                $rectify = '<a onclick=rectify('.$script_href.'); class="btn btn-danger"><i class="fa fa-undo" alt="Rectificar"> RECTIFICAR</i></a>';
+                $rectify = '<a onclick=javascript:rectify('.$script_href.'); class="btn btn-danger"><i class="fa fa-undo" alt="Rectificar"> RECTIFICAR</i></a>';
                 $list_files .= "<li>" . $download . " " . $print_file . " " . $rectify . " " . $print_filename . "  [" . $file['period'] . "][" . $file['status'] . "] </li>";
                 
             }
