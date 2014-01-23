47b13c357172a1594648c577d8de5443c3b8cf7f
Diego <daotero@industria.gob.ar>
2014-01-23 12:17:11 -0300

    Fix SGR  redirect

--- application/modules/sgr/controllers/sgr.php
+++ application/modules/sgr/controllers/sgr.php
@@ -608,7 +608,7 @@ class Sgr extends MX_Controller {
                 
                 
                 $rectify_count_each = ($count > 0) ? "- " . $count . "º RECTIFICATIVA" : "";
-                $download = anchor('sgr/xls_asset/124/' . $file['filename'], ' <i class="fa fa-download" alt="Descargar"></i>', array('class' => 'btn btn-primary' . $disabled_link));
+                $download = anchor('sgr/xls_asset/'.$anexo.'/' . $file['filename'], ' <i class="fa fa-download" alt="Descargar"></i>', array('class' => 'btn btn-primary' . $disabled_link));
                 $print_file = anchor('/sgr/print_anexo/' . $file['filename'], ' <i class="fa fa-print" alt="Imprimir"></i>', array('target' => '_blank', 'class' => 'btn btn-primary' . $disabled_link));
                 $rectifica_link_class = ($this->session->userdata['period']) ? 'rectifica-warning_' . $file['period'] : 'rectifica-link_' . $file['period'];
                 $rectify = anchor($file['period'] . "/" . $anexo, '<i class="fa fa-undo" alt="Rectificar"> RECTIFICAR</i>', array('class' => $rectifica_link_class . ' btn btn-danger'));
