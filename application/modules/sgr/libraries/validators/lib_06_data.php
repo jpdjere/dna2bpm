ef22989acd4a31128f69bc9d42a89888e10510f7
Vaio <Vaio@Vaio-PC>
2014-02-01 17:19:57 -0300

    Support #8667 OBSERVACIONES SOBRE VALIDACIONES ANEXO 6

--- application/modules/sgr/libraries/validators/lib_06_data.php
+++ application/modules/sgr/libraries/validators/lib_06_data.php
@@ -297,26 +297,21 @@ class Lib_06_data extends MX_Controller {
                                 20 => 'T.2',
                                 21 => 'U.2',
                                 22 => 'V.2',
-                            );
-
-                            $is_empty_arr2 = array(
                                 23 => 'W.2',
                                 24 => 'X.2',
-                                25 => 'Y.2',
                                 26 => 'Z.2',
                                 28 => 'AB.1'
                             );
+                            $ord_arr = array();
+
                             foreach ($is_empty_arr as $col_num => $error_code) {
                                 if (false !== ($pos = array_search2d($error_code, $stack))) {
-                                    echo $error_code . " found at index " . $pos . "<br/>";
-                                    unset($stack[$pos]);
+                                    $ord_arr[] = $pos;
                                 }
                             }
-                            foreach ($is_empty_arr2 as $col_num => $error_code) {
-                                if (false !== ($pos = array_search2d($error_code, $stack))) {
-                                    echo $error_code . " found at index " . $pos . "<br/>";
-                                    unset($stack[$pos]);
-                                }
+
+                            foreach ($ord_arr as $ord_num) {
+                                unset($stack[$ord_num]);
                             }
                         }
                     }
@@ -1445,7 +1440,7 @@ class Lib_06_data extends MX_Controller {
                 }
             }
         }
-        
+        exit();
         $this->data = $stack;
     }
 
