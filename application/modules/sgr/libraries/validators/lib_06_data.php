e52b30e2f3c04a87739d81db665207441aa5e961
Vaio <Vaio@Vaio-PC>
2014-02-01 12:57:17 -0300

    Support #8667 OBSERVACIONES SOBRE VALIDACIONES ANEXO 6

--- application/modules/sgr/libraries/validators/lib_06_data.php
+++ application/modules/sgr/libraries/validators/lib_06_data.php
@@ -1416,7 +1416,7 @@ class Lib_06_data extends MX_Controller {
                             $is_empty_arr = array(17 => 'S.2', 19 => 'T.2');
                             foreach ($is_empty_arr as $col_num => $error_code) {
                                 foreach ($stack as $value) {
-                                    if (in_array('R.2', $stack[0])) {
+                                    if (in_array($error_code, $stack[0])) {
                                         unset($stack[0]);
                                     }
 
