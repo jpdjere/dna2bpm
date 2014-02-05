bbb4ad4c8b527b3ea7e7e9a9d3cce0eeaa6452fd
Diego <daotero@industria.gob.ar>
2014-02-05 15:13:56 -0300

    Support #8624: OBSERVACIONES ANEXO 06

--- application/modules/sgr/views/success.php
+++ application/modules/sgr/views/success.php
@@ -1,8 +1,6 @@
 <div class="row-fluid test" id="barra_user" > 
     <ul class="breadcrumb" style="margin-bottom:0px;padding-bottom:0px" >
-        <button type="button" class="btn hide_offline" data-toggle="collapse" data-target="#file_div">
-            <i class="fa fa-plus"></i>  Seleccionar Archivos a Procesar
-        </button> 
+        <i class="fa fa-backward"></i><a href="../../sgr/"> Volver</a> | <i class="fa fa-print"></i> <a href="javascript:window.print()">Imprimir pantalla</a>
             
         <li class="pull-right perfil">
             SGR: {sgr_nombre}  <span id="status"> <i class="{rol_icono}"></i> {username} [Grupo: {rol}]</span>
@@ -14,7 +12,7 @@
 <div class="container" > 
     <h1 class="success">Información sobre la importación del Anexo</h1>
     <div class="alert alert-success" id="{_id}">       
-        <i class="fa fa-backward"></i><a href="../../sgr/"> Volver</a> | <i class="fa fa-print"></i> <a href="javascript:window.print()">Imprimir pantalla</a>   </p> 
+            
         <h3>El Anexo se proceso correctamente.</h3>
         <h4>Periodo informado: {sgr_period}</h4>
         <p> </p>
