3c5157963030737c0ff039e99c0a4cab78aab45b
Vaio <Vaio@Vaio-PC>
2014-02-11 11:04:38 -0300

    Support #8639 OBSERVCIONES ANEXO 12

--- application/modules/sgr/libraries/validators/lib_12_data.php
+++ application/modules/sgr/libraries/validators/lib_12_data.php
@@ -784,6 +784,8 @@ class Lib_12_data extends MX_Controller {
                 }
             } // END FOR LOOP->
         }
+        var_dump($stack);
+        exit();
         $this->data = $stack;
     }
 
