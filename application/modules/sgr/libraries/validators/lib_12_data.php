88460907aafce5e11c90cb156e32dc0428927e3a
Vaio <Vaio@Vaio-PC>
2014-02-11 14:43:54 -0300

    Support #8639 OBSERVCIONES ANEXO 12

--- application/modules/sgr/libraries/validators/lib_12_data.php
+++ application/modules/sgr/libraries/validators/lib_12_data.php
@@ -97,10 +97,16 @@ class Lib_12_data extends MX_Controller {
                         }
                     }
 
-                    $year_period =  $this->session->userdata['period'];
+                   
                     echo $year_period . "<br>";
+                    
                     $partner_data = $this->$model_06->get_partner($parameterArr[$i]['fieldValue']);
-                    if ($partner_data['FECHA_DE_TRANSACCION']) {
+                    list($month_period,$year_period) =  explode("-",$this->session->userdata['period']);
+                    $transaction_year = explode("-",$partner_data['FECHA_DE_TRANSACCION']);
+                    
+                    $result_dates = (int)$year_period-(int)$transaction_year;
+                    var_dump($result_dates);
+                    if ($result_dates<=0) {
                         $code_error = "B.2";
                         $result["error_code"] = $code_error;
                         $result["error_row"] = $parameterArr[$i]['row'];
