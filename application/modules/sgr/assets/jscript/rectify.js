46fed2ceeeb13560fbfeb0cb928d4d8a3cbecb59
Vaio <Vaio@Vaio-PC>
2014-02-15 15:08:55 -0300

    Fix index

--- application/modules/sgr/assets/jscript/rectify.js
+++ application/modules/sgr/assets/jscript/rectify.js
@@ -4,6 +4,17 @@
  */
 
 $(document).ready(function() {
+    $('[id^="others_"]').hide();
+    /*RECTIFICAR*/
+    $('[id^="rectify_"]').change(function() {
+        var option_value = $(this).val();
+        if (option_value == 3) {
+            $('[id^="others_"]').show();
+        } else {
+            $('[id^="others_"]').hide();
+        }
+    });
+    
     $("#error").html('<i class="fa fa-info-circle"></i> Si rectifica, la información asociada y relacionada será borrada del sistema');
     $("#is_session").show();
     $("#no_session").hide();
