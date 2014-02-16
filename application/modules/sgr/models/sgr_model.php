8005cb32b36acec05f223777067f00cec21d9c65
Vaio <Vaio@Vaio-PC>
2014-02-16 16:03:36 -0300

    Support #9235 ANEXO 06 - CONDICIÓN PYME

--- application/modules/sgr/models/sgr_model.php
+++ application/modules/sgr/models/sgr_model.php
@@ -265,7 +265,7 @@ class Sgr_model extends CI_Model {
         $query = array("code" => $regex);
         $fields = array("sector", "code");
         $result = $this->mongo->sgr->$container->findOne($query, $fields);
-        var_dump($query,$result['sector']);
+        //var_dump($query,$result['sector']);
         return $result['sector'];
     }
 
