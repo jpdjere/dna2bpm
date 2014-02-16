0c171f75c5cc3b801821b686a3fd38c63952cb35
Vaio <Vaio@Vaio-PC>
2014-02-16 13:32:00 -0300

    Support #9235 ANEXO 06 - CONDICIÓN PYME

--- application/modules/sgr/libraries/validators/lib_06_data.php
+++ application/modules/sgr/libraries/validators/lib_06_data.php
@@ -393,6 +393,8 @@ class Lib_06_data extends MX_Controller {
 
                     $get_ciu = ciu($ciu);//ciu(cerosClanae($ciu));
                     $sector = ($get_ciu) ? $get_ciu : $this->sgr_model->clae2013($ciu);
+                    
+                   
 
                     if (!$sector) {
                         if ($A1_field_value == "INCORPORACION") {
