c5c295662122a8469d5b3c5c196b12a79c87ea3a
Vaio <Vaio@Vaio-PC>
2014-02-16 13:43:09 -0300

    Support #9235 ANEXO 06 - CONDICIÓN PYME

--- application/modules/sgr/libraries/validators/lib_06_data.php
+++ application/modules/sgr/libraries/validators/lib_06_data.php
@@ -1300,8 +1300,8 @@ class Lib_06_data extends MX_Controller {
                 }
             }
         }
-//        var_dump($stack);
-//        exit();
+        var_dump($stack);
+        exit();
         $this->data = $stack;
     }
 
