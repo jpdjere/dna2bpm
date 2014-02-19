85b928e888ed568612a8e4417b3c422e1da82f52
Diego <daotero@industria.gob.ar>
2014-02-19 11:40:10 -0300

    Support #9306 ANEXO 06.1 - VALIDADOR A.1

--- application/modules/sgr/libraries/validators/lib_061_data.php
+++ application/modules/sgr/libraries/validators/lib_061_data.php
@@ -35,6 +35,7 @@ class Lib_061_data extends MX_Controller {
             /* Validacion Basica */
             for ($i = 0; $i <= count($parameterArr); $i++) {
 
+
                 /* CUIT_SOCIO_INCORPORADO
                  * Nro A.1
                  * Detail:
@@ -45,8 +46,18 @@ class Lib_061_data extends MX_Controller {
                  */
 
                 if ($parameterArr[$i]['col'] == 1) {
+                    $A1_field_value = ($parameterArr[$i]['fieldValue'])?$parameterArr[$i]['fieldValue'] : 0;                    
+                    $code_error = "A.1";
+                    //empty field Validation
+                    $return = check_empty($parameterArr[$i]['fieldValue']);                        
+                    if ($return) {
+                        $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
+                        array_push($stack, $result);
+                        $A1_field_value = false;
+                    }
 
-                    $A1_field_value = $parameterArr[$i]['fieldValue'];
+
+
                     $code_error = "A.2";
                     $partner_data = $this->$model_06->get_partner($parameterArr[$i]['fieldValue'], $this->session->userdata['period']);
                     if (!$partner_data) {
@@ -54,217 +65,208 @@ class Lib_061_data extends MX_Controller {
                         array_push($stack, $result);
                     }
                 }
-            }
+                
 
-            /* TIENE_VINCULACION
-             * Nro B.1
-             * Detail:
-             * El campo no puede estar vacío y debe contener uno de los siguientes parámetros:
-              SI
-              NO
-             * Nro B.2
-             * Detail:
-             * Si el CUIT informado en la Columna A comienza con 30 o 33 (Correspondiente a Personas Jurídicas) la opción debe ser “SI”. 
-             * Nro B.3
-             * Detail:
-             * Si se indica la opción “NO” el CUIT no puede estar más de una vez en la Columna A de este Anexo,  y las Columnas C, D, E, y F deben estar vacías.
-             */
+                /* TIENE_VINCULACION
+                 * Nro B.1
+                 * Detail:
+                 * El campo no puede estar vacío y debe contener uno de los siguientes parámetros:
+                  SI
+                  NO
+                 * Nro B.2
+                 * Detail:
+                 * Si el CUIT informado en la Columna A comienza con 30 o 33 (Correspondiente a Personas Jurídicas) la opción debe ser “SI”. 
+                 * Nro B.3
+                 * Detail:
+                 * Si se indica la opción “NO” el CUIT no puede estar más de una vez en la Columna A de este Anexo,  y las Columnas C, D, E, y F deben estar vacías.
+                 */
 
-            if ($parameterArr[$i]['col'] == 2) {
+                if ($parameterArr[$i]['col'] == 2) {
 
-                $code_error = "B.1";
-
-                //empty field Validation
-                $return = check_empty($parameterArr[$i]['fieldValue']);
-                if ($return) {
-                    $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
-                    array_push($stack, $result);
-                }
-                //Value Validation
-                if (isset($parameterArr[$i]['fieldValue'])) {
-                    $B1_field_value = "";
-                    $allow_words = array("SI", "NO");
-                    $return = check_word($parameterArr[$i]['fieldValue'], $allow_words);
-                    if ($return) {
-                        $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
-                        array_push($stack, $result);
-                    } else {
-                        $B1_field_value = $parameterArr[$i]['fieldValue'];
-                    }
-                }
-
-                $code_error = "B.2";
-            }
-
-            /* CUIT_VINCULADO
-             * Nro C.1
-             * Detail:
-             * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío y  debe tener 11 caracteres sin guiones. El CUIT debe cumplir el “ALGORITMO VERIFICADOR”.
-             */
-            if ($parameterArr[$i]['col'] == 3) {
-
-                $code_error = "C.1";
-                //empty field Validation
-                if ($B1_field_value == "SI") {
-                    $return = check_empty($parameterArr[$i]['fieldValue']);
-                    if ($return) {
-                        $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
-                        array_push($stack, $result);
-                    }
-                }
-
-                if ($parameterArr[$i]['fieldValue'] != "") {
-                    $return = cuit_checker($parameterArr[$i]['fieldValue']);
-                    if (!$return) {
-                        $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
-                        array_push($stack, $result);
-                    }
-                }
-            }
-
-            /* RAZON_SOCIAL_VINCULADO
-             * Nro D.1
-             * Detail:
-             * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío. Si el CUIT se encuentra registrado en la Base de Datos del Sistema, debe tomar el nombre allí escrito, de lo contrario, debe tomar transitoriamente el nombre informado por la SGR.
-             */
-            if ($parameterArr[$i]['col'] == 4) {
-                $code_error = "D.1";
-                //Check Empry
-                if ($B1_field_value == "SI") {
-                    $return = check_empty($parameterArr[$i]['fieldValue']);
-                    if ($return) {
-
-
-                        $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
-                        array_push($stack, $result);
-                    }
-                }
-            }
-
-            /* TIPO_RELACION_VINCULACION
-             * Nro E.1
-             * Detail:
-             * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío y debe contener uno de los siguientes parámetros:
-              ASCENDENTE
-              DESCENDENTE
-             */
-            if ($parameterArr[$i]['col'] == 5) {
-                $code_error = "D.1";
-                //Check Empry
-                if ($B1_field_value == "SI") {
-                    $code_error = "E.1";
+                    $code_error = "B.1";
 
                     //empty field Validation
                     $return = check_empty($parameterArr[$i]['fieldValue']);
                     if ($return) {
-
-
                         $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
                         array_push($stack, $result);
                     }
                     //Value Validation
                     if (isset($parameterArr[$i]['fieldValue'])) {
                         $B1_field_value = "";
-                        $allow_words = array("ASCENDENTE", "DESCENDENTE");
+                        $allow_words = array("SI", "NO");
                         $return = check_word($parameterArr[$i]['fieldValue'], $allow_words);
+                        if ($return) {
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
+                            array_push($stack, $result);
+                        } else {
+                            $B1_field_value = $parameterArr[$i]['fieldValue'];
+                        }
+                    }
+
+                    $code_error = "B.2";
+                }
+
+                /* CUIT_VINCULADO
+                 * Nro C.1
+                 * Detail:
+                 * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío y  debe tener 11 caracteres sin guiones. El CUIT debe cumplir el “ALGORITMO VERIFICADOR”.
+                 */
+                if ($parameterArr[$i]['col'] == 3) {
+
+                    $code_error = "C.1";
+                    //empty field Validation
+                    if ($B1_field_value == "SI") {
+                        $return = check_empty($parameterArr[$i]['fieldValue']);
+                        if ($return) {
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
+                            array_push($stack, $result);
+                        }
+                    }
+
+                    if ($parameterArr[$i]['fieldValue'] != "") {
+                        $return = cuit_checker($parameterArr[$i]['fieldValue']);
+                        if (!$return) {
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
+                            array_push($stack, $result);
+                        }
+                    }
+                }
+
+                /* RAZON_SOCIAL_VINCULADO
+                 * Nro D.1
+                 * Detail:
+                 * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío. Si el CUIT se encuentra registrado en la Base de Datos del Sistema, debe tomar el nombre allí escrito, de lo contrario, debe tomar transitoriamente el nombre informado por la SGR.
+                 */
+                if ($parameterArr[$i]['col'] == 4) {
+                    $code_error = "D.1";
+                    //Check Empry
+                    if ($B1_field_value == "SI") {
+                        $return = check_empty($parameterArr[$i]['fieldValue']);
                         if ($return) {
 
 
-                            $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
-                            array_push($stack, $result);
-                        }
-                    }
-
-
-                    /* TIPO_RELACION_VINCULACION
-                     * Nro E.2
-                     * Detail:
-                     * Si el número de CUIT informado en la Columna A empieza con 20, 23 o 27 
-                     * (los tres correspondientes a personas físicas), y se indicó que el Socio SI tiene Relaciones de Vinculación (Columna B), 
-                     * la opción elegida sólo puede ser DESCENDENTE.
-                     */
-                    $code_error = "E.2";
-                    $check_cuit = substr($A1_field_value, 0, 2);
-                    $opt_arr = array('20', '23', '27');
-                    $pos = strpos($check_cuit, $findme);
-
-                    if (in_array($check_cuit, $opt_arr)) {
-                        if ($parameterArr[$i]['fieldValue'] != "DESCENDENTE") {
-                            $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
                             array_push($stack, $result);
                         }
                     }
                 }
-            }
 
-            /* TIPO_RELACION_VINCULACION
-             * Nro F.1
-             * Detail:
-             * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío.
-             */
-            if ($parameterArr[$i]['col'] == 6) {
-                $code_error = "F.1";
+                /* TIPO_RELACION_VINCULACION
+                 * Nro E.1
+                 * Detail:
+                 * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío y debe contener uno de los siguientes parámetros:
+                  ASCENDENTE
+                  DESCENDENTE
+                 */
+                if ($parameterArr[$i]['col'] == 5) {
 
-                if ($B1_field_value == "SI") {
-                    //empty field Validation
-                    $return = check_empty($parameterArr[$i]['fieldValue']);
+                    $code_error = "D.1";
+                    //Check Empry
+                    if ($B1_field_value == "SI") {
+                        $code_error = "E.1";
+
+                        //empty field Validation
+                        $return = check_empty($parameterArr[$i]['fieldValue']);
+                        if ($return) {
+
+
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
+                            array_push($stack, $result);
+                        }
+                        //Value Validation
+                        if (isset($parameterArr[$i]['fieldValue'])) {
+                            $B1_field_value = "";
+                            $allow_words = array("ASCENDENTE", "DESCENDENTE");
+                            $return = check_word($parameterArr[$i]['fieldValue'], $allow_words);
+                            if ($return) {
+
+
+                                $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
+                                array_push($stack, $result);
+                            }
+                        }
+
+
+                        /* TIPO_RELACION_VINCULACION
+                         * Nro E.2
+                         * Detail:
+                         * Si el número de CUIT informado en la Columna A empieza con 20, 23 o 27 
+                         * (los tres correspondientes a personas físicas), y se indicó que el Socio SI tiene Relaciones de Vinculación (Columna B), 
+                         * la opción elegida sólo puede ser DESCENDENTE.
+                         */
+                        $code_error = "E.2";
+                        $check_cuit = substr($A1_field_value, 0, 2);
+                        $opt_arr = array('20', '23', '27');
+                        $pos = strpos($check_cuit, $findme);
+
+                        if (in_array($check_cuit, $opt_arr)) {
+                            if ($parameterArr[$i]['fieldValue'] != "DESCENDENTE") {
+                                $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
+                                array_push($stack, $result);
+                            }
+                        }
+                    }
+                }
+
+                /* TIPO_RELACION_VINCULACION
+                 * Nro F.1
+                 * Detail:
+                 * Si en la Columna B se completó la opción “SI”, el campo no puede estar vacío.
+                 */
+                if ($parameterArr[$i]['col'] == 6) {
+
+                    $code_error = "F.1";
+
+                    if ($B1_field_value == "SI") {
+                        //empty field Validation
+                        $return = check_empty($parameterArr[$i]['fieldValue']);
+                        if ($return) {
+
+
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
+                            array_push($stack, $result);
+                        }
+                    }
+                    $code_error = "F.2";
+                    $return = check_is_numeric($parameterArr[$i]['fieldValue']);
                     if ($return) {
 
 
-                        $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
+                        $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
                         array_push($stack, $result);
                     }
-                }
-                $code_error = "F.2";
-                $return = check_is_numeric($parameterArr[$i]['fieldValue']);
-                if ($return) {
 
-
-                    $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
-                    array_push($stack, $result);
-                } 
-                
-                /* COLUMN A */
-                $code_error = "A.1";
-                //empty field Validation
-                $return = check_empty($A1_field_value);
-                var_dump("-->", $return, $A1_field_value);
-                if ($return) {
-                    $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
-                    array_push($stack, $result);
-                } else {
-
-                    $count_inc[] = $A1_field_value;
-
-                    $return = cuit_checker($A1_field_value);
-                    if (!$return) {
-                        $result = return_error_array($code_error, $parameterArr[$i]['row'], $A1_field_value);
-                        array_push($stack, $result);
+                    if ($A1_field_value) {
+                        $code_error = "A.1";
+                        $count_inc[] = $A1_field_value;
+                        $return = cuit_checker($A1_field_value);
+                        if (!$return) {
+                            $result = return_error_array($code_error, $parameterArr[$i]['row'], $A1_field_value);
+                            array_push($stack, $result);
+                        }
                     }
                 }
-                
+            }
+
+            /* CUIT_SOCIO_INCORPORADO
+             * Nro A.3
+             * Detail:
+             * Todos los Socios que fueron informados como Incorporados en el Anexo 6 – Movimientos de Capital Social, deben figurar en esta columna.
+             */
+            $partners_error_data = $this->$model_06->new_count_partners($count_inc, $this->session->userdata['period']);
+            if ($partners_error_data) {
+                $stack = array();
+                $code_error = "A.3";
+
+                $result["error_row"] = 1;
+                $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
+                array_push($stack, $result);
             }
         }
-
-        /* CUIT_SOCIO_INCORPORADO
-         * Nro A.3
-         * Detail:
-         * Todos los Socios que fueron informados como Incorporados en el Anexo 6 – Movimientos de Capital Social, deben figurar en esta columna.
-         */
-        $partners_error_data = $this->$model_06->new_count_partners($count_inc, $this->session->userdata['period']);
-        if ($partners_error_data) {
-            $stack = array();
-            $code_error = "A.3";
-
-            $result["error_row"] = 1;
-            $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
-            array_push($stack, $result);
-        }
-
-        var_dump($stack);
-        exit();
+//        var_dump($stack);
+//        exit();
         $this->data = $stack;
     }
 
 }
-
