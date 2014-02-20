b30c93d5c7f1c292874f5c0baac206a7cfaa3ef5
Vaio <Vaio@Vaio-PC>
2014-02-20 17:42:00 -0300

    Support #9214: ANEXO 06.1 - VALIDADOR A.3 ANEXO 06.1 - VALIDADOR A.2

--- application/modules/sgr/libraries/validators/lib_061_data.php
+++ application/modules/sgr/libraries/validators/lib_061_data.php
@@ -62,12 +62,9 @@ class Lib_061_data extends MX_Controller {
 
                     $code_error = "A.2";
                     $partner_data = $this->$model_06->get_partner_period($parameterArr[$i]['fieldValue'], $this->session->userdata['period']);
-                    var_dump($partner_data);
-                     foreach ($partner_data as $partner){                       
-                        $transaction_date = $partner[5779];
-                    }
-                    
-                    if ($partner[5779]!=1) {
+                    var_dump($partner_data[5779]);
+                     
+                    if ($partner_data[5779]!=1) {
                         $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
                         array_push($stack, $result);
                     }
