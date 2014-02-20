f6ef91c4b731666285c52ff14bba4aff6a5884ad
Vaio <Vaio@Vaio-PC>
2014-02-20 20:11:03 -0300

    FIX XLS numeros negativos function _GetInt4d

--- application/modules/sgr/libraries/excel_reader2.php
+++ application/modules/sgr/libraries/excel_reader2.php
@@ -73,8 +73,7 @@ function GetInt4d_old($data, $pos) {
 }
 
 function GetInt4d($data, $pos) {
-        $value = ord($data[$pos]) | (ord($data[$pos + 1]) << 8) | (ord($data[$pos + 2]) <<
-                16) | (ord($data[$pos + 3]) << 24);
+        $value = ord($data[$pos]) | (ord($data[$pos + 1]) << 8) | (ord($data[$pos + 2]) << 16) | (ord($data[$pos + 3]) << 24);
         if ($value >= 4294967294) {
             $value = -2;
         }
