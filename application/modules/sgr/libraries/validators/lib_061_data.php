6662e01267daf0a050a35c8fa57cc22259134e86
Vaio <Vaio@Vaio-PC>
2014-02-20 17:44:18 -0300

    Support #9214: ANEXO 06.1 - VALIDADOR A.3 ANEXO 06.1 - VALIDADOR A.2

--- application/modules/sgr/libraries/validators/lib_061_data.php
+++ application/modules/sgr/libraries/validators/lib_061_data.php
@@ -63,7 +63,7 @@ class Lib_061_data extends MX_Controller {
                     $code_error = "A.2";
                     $partner_data = $this->$model_06->get_partner_period($parameterArr[$i]['fieldValue'], $this->session->userdata['period']);
                     
-                    if ($partner_data[5779]!=1) {
+                    if ($partner_data[5779]!='1') {
                         $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
                         array_push($stack, $result);
                     }
@@ -267,7 +267,7 @@ class Lib_061_data extends MX_Controller {
                 array_push($stack, $result);
             }
         }
-        var_dump($stack);        exit();
+        //var_dump($stack);        exit();
         $this->data = $stack;
     }
 
