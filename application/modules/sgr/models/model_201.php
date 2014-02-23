77830d6b88580b97bc8336086a85741d906f2a63
Vaio <Vaio@Vaio-PC>
2014-02-23 14:06:24 -0300

    Support #9475 ANEXO 20.1 - VALIDADOR C.1

--- application/modules/sgr/models/model_201.php
+++ application/modules/sgr/models/model_201.php
@@ -315,7 +315,7 @@ class Model_201 extends CI_Model {
 
 //$new_result = $this->mongo->sgr->$container->find(array('NUMERO_DE_APORTE' => $code))->sort(array('NUMERO_DE_APORTE' => -1))->limit(1);
 $new_result = $this->mongo->db->apps->find($query)->sort(array('NUMERO_DE_APORTE' => -1));
-foreach ($result as $list) {
+foreach ($new_result as $list) {
 var_dump($list);
 }
 //            if ($new_result) {
