f1f092121ab70d91fb30ccafa79f2bd580fb2424
Vaio <Vaio@Vaio-PC>
2014-02-23 12:20:38 -0300

    Support #9477 Anexo 12.2 - Validador B.1

--- application/modules/sgr/helpers/tools_helper.php
+++ application/modules/sgr/helpers/tools_helper.php
@@ -489,3 +489,46 @@ function count_shares($data) {
     $group['total_shares'] = array("shares" => $final_shares);
     return $group;
 }
+
+
+function repeatedElements($array, $returnWithNonRepeatedItems = false)
+{
+    $repeated = array();
+ 
+    foreach( (array)$array as $value )
+    {
+        $inArray = false;
+ 
+        foreach( $repeated as $i => $rItem )
+        {
+            if( $rItem['value'] === $value )
+            {
+                $inArray = true;
+                ++$repeated[$i]['count'];
+            }
+        }
+ 
+        if( false === $inArray )
+        {
+            $i = count($repeated);
+            $repeated[$i] = array();
+            $repeated[$i]['value'] = $value;
+            $repeated[$i]['count'] = 1;
+        }
+    }
+ 
+    if( ! $returnWithNonRepeatedItems )
+    {
+        foreach( $repeated as $i => $rItem )
+        {
+            if($rItem['count'] === 1)
+            {
+                unset($repeated[$i]);
+            }
+        }
+    }
+ 
+    sort($repeated);
+ 
+    return $repeated;
+}
