10ca4a98d2fc75daf400278b0edc35a7341388df
Vaio <Vaio@Vaio-PC>
2014-03-04 18:47:28 -0300

    Support #9616 Anexo 12.3 - Validador A.1

--- application/modules/sgr/libraries/validators/lib_123_data.php
+++ application/modules/sgr/libraries/validators/lib_123_data.php
@@ -108,7 +108,7 @@ class Lib_123_data extends MX_Controller {
                             $code_error = "B.2";
                             $result = return_error_array($code_error, $parameterArr[$i]['row'], "empty");
                             array_push($stack, $result);
-                        } else if ($parameterArr[$cell]['fieldValue'] > $amount) {
+                        } else if ($parameterArr[$cell]['fieldValue'] >= $amount) {
                             $code_error = "B.1";
                             $result = return_error_array($code_error, $parameterArr[$cell]['row'], $parameterArr[$cell]['fieldValue']);
                             array_push($stack, $result);
