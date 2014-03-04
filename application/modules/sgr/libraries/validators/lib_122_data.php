6d73c5d3c736af5d8a1d3d4181efe703be11219d
Vaio <Vaio@Vaio-PC>
2014-03-04 17:05:39 -0300

    Support #9600 Anexo 12.2 - Validador F.1

--- application/modules/sgr/libraries/validators/lib_122_data.php
+++ application/modules/sgr/libraries/validators/lib_122_data.php
@@ -123,7 +123,7 @@ class Lib_122_data extends MX_Controller {
                         
                         
                         if ($result_dates < 1) {
-                            var_dump($nro_orden['5215'], $C_cell_date_format);
+                            var_dump($nro_orden['5215'], $C_cell_date_format, $result_dates, "<hr>");
                             $code_error = "C.2";
                             $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
                             array_push($stack, $result);
