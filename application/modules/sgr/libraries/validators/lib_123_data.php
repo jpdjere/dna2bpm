93f796b8b0d412677570a6a43b4907a9dde5acfb
Vaio <Vaio@Vaio-PC>
2014-03-04 17:39:36 -0300

    Support #9616 Anexo 12.3 - Validador A.1

--- application/modules/sgr/libraries/validators/lib_123_data.php
+++ application/modules/sgr/libraries/validators/lib_123_data.php
@@ -120,6 +120,8 @@ class Lib_123_data extends MX_Controller {
                 }
             } // END FOR LOOP->
         }
+        var_dump($stack);
+        exit();
         $this->data = $stack;
     }
 
