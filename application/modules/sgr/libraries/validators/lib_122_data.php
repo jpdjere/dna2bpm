7e450230c19f31ead0c7ce8f576a2c5b82c0572a
Vaio <Vaio@Vaio-PC>
2014-03-04 16:24:15 -0300

    Support #9592 Anexo 12.2- Validador A.1

--- application/modules/sgr/libraries/validators/lib_122_data.php
+++ application/modules/sgr/libraries/validators/lib_122_data.php
@@ -112,7 +112,7 @@ class Lib_122_data extends MX_Controller {
                         /* C.2 */
                         $C_cell_date_format = strftime("%Y-%m-%d", mktime(0, 0, 0, 1, -1 + $parameterArr[$i]['fieldValue'], 1900));
                         
-                        var_dump($warranty_info, $C_cell_date_format);
+                        var_dump($warranty_info['5215'], $C_cell_date_format);
                         
                         $datetime1 = new DateTime($warranty_info['5215']);
                         $datetime2 = new DateTime($C_cell_date_format);
@@ -222,7 +222,7 @@ class Lib_122_data extends MX_Controller {
         }
 
 
-        exit();
+        //exit();
         $this->data = $stack;
     }
 
