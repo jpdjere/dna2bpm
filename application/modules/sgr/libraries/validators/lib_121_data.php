6bbfcfe25ac32f24506a4f4138298ea0c01f510b
gabriel <gabriel@gabriel-VGN-N350FE>
2014-03-06 12:19:02 -0300

    SGR Helpers check_decimal , fix de funcion

--- application/modules/sgr/libraries/validators/lib_121_data.php
+++ application/modules/sgr/libraries/validators/lib_121_data.php
@@ -147,7 +147,7 @@ class Lib_121_data extends MX_Controller {
                     }
 
                     if (isset($parameterArr[$i]['fieldValue'])) {
-                        $return = check_decimal($parameterArr[$i]['fieldValue']);
+                        $return = check_decimal($parameterArr[$i]['fieldValue'],2,true);
                         if ($return) {
 
                             $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
@@ -155,10 +155,7 @@ class Lib_121_data extends MX_Controller {
                         }
                     }
                     
-//                    if(check_decimal($parameterArr[$i]['fieldValue'],null,true)){
-//                            $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
-//                            array_push($stack, $result);
-//                    }
+
 
                     //Valida contra Mongo
                 }
@@ -183,8 +180,6 @@ class Lib_121_data extends MX_Controller {
                     if (isset($parameterArr[$i]['fieldValue'])) {
                         $return = check_decimal($parameterArr[$i]['fieldValue']);
                         if ($return) {
-
-
                             $result = return_error_array($code_error, $parameterArr[$i]['row'], $parameterArr[$i]['fieldValue']);
                             array_push($stack, $result);
                         }
