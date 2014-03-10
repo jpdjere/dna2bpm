b8b1a52820a33ee682c44e8cd62f11cbf240e502
Diego <daotero@industria.gob.ar>
2014-03-10 17:58:51 -0300

    Support #9596 Anexo 12.2 - Validador C.2

--- application/modules/sgr/models/model_122.php
+++ application/modules/sgr/models/model_122.php
@@ -27,7 +27,7 @@ class Model_122 extends CI_Model {
             $this->sgr_nombre = $sgr['1693'];
         }
     }
-    
+
     function sanitize($parameter) {
         /* FIX INFORMATION */
         $parameter = (array) $parameter;
@@ -63,14 +63,14 @@ class Model_122 extends CI_Model {
         $insertarr = array();
         foreach ($defdna as $key => $value) {
             $insertarr[$value] = $parameter[$key];
-            /*STRING*/
+            /* STRING */
             $insertarr['NRO_GARANTIA'] = (string) $insertarr['NRO_GARANTIA'];
-            
-            /*INT & FLOAT*/
+
+            /* INT & FLOAT */
             $insertarr['NUMERO_CUOTA_CUYO_VENC_MODIFICA'] = (int) $insertarr['NUMERO_CUOTA_CUYO_VENC_MODIFICA'];
-            
+
             $insertarr['MONTO_CUOTA'] = (float) $insertarr['MONTO_CUOTA'];
-            $insertarr['SALDO_AL_VENCIMIENTO'] = (float) $insertarr['SALDO_AL_VENCIMIENTO'];            
+            $insertarr['SALDO_AL_VENCIMIENTO'] = (float) $insertarr['SALDO_AL_VENCIMIENTO'];
         }
         return $insertarr;
     }
@@ -78,10 +78,10 @@ class Model_122 extends CI_Model {
     function save($parameter) {
         $period = $this->session->userdata['period'];
         $container = 'container.sgr_anexo_' . $this->anexo;
-        
+
         $parameter['FECHA_VENC_CUOTA'] = new MongoDate(strtotime(translate_for_mongo($parameter['FECHA_VENC_CUOTA'])));
         $parameter['FECHA_VENC_CUOTA_NUEVA'] = new MongoDate(strtotime(translate_for_mongo($parameter['FECHA_VENC_CUOTA_NUEVA'])));
-        
+
         $parameter['period'] = $period;
         $parameter['origin'] = 2013;
 
@@ -96,7 +96,6 @@ class Model_122 extends CI_Model {
         }
         return $out;
     }
-    
 
     function save_period($parameter) {
         /* ADD PERIOD */
@@ -142,19 +141,87 @@ class Model_122 extends CI_Model {
         return $rs['err'];
     }
 
-    function get_anexo_info($anexo, $parameter) {
+    function get_anexo_info($anexo, $parameter, $xls = false) {
+        $tmpl = array(
+            'data' => '<tr><td align="center" rowspan="2">N° de<br>Garntía</td>
+                                <td align="center" rowspan="2">N° de Cuota<br>cuyo vencimiento se<br>modifica</td>
+                                <td align="center" colspan="2">Del Participe/Beneficiario </td>
+                                <td align="center" rowspan="2">Fecha de Origen de la Garantía</td>                                
+                                <td align="center" colspan="3">De la Cuota cuyo vencimiento se modifica</td>
+                                <td align="center" rowspan="2">Saldo al Vencimiento</td> 
+    <tr>
+        <td>Nombre o Razón Social</td>
+        <td>C.U.I.T</td>
+        <td>Fecha de Vencimiento Original</td>
+        <td>Fecha de Efectiva Cancelación</td>
+        <td>Monto</td>      
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+    </tr> ',
+        );
 
-        $headerArr = array("NRO_GARANTIA", "NUMERO_CUOTA_CUYO_VENC_MODIFICA", "FECHA_VENC_CUOTA", "FECHA_VENC_CUOTA_NUEVA", "MONTO_CUOTA", "SALDO_AL_VENCIMIENTO");
-        $data = array($headerArr);
+        $data = array($tmpl);
         $anexoValues = $this->get_anexo_data($anexo, $parameter);
         foreach ($anexoValues as $values) {
             $data[] = array_values($values);
         }
-        $this->load->library('table');
-        return $this->table->generate($data);
+        $this->load->library('table_custom');
+        $newTable = $this->table_custom->generate($data);
+        return $newTable;
     }
 
-    function get_anexo_data($anexo, $parameter) {
+    function get_anexo_data($anexo, $parameter, $xls = false) {
+        header('Content-type: text/html; charset=UTF-8');
+        $rtn = array();
+        $container = 'container.sgr_anexo_' . $anexo;
+        $query = array("filename" => $parameter);
+        $result = $this->mongo->sgr->$container->find($query);
+
+        foreach ($result as $list) {
+            /* Vars */
+            $cuit = str_replace("-", "", $list['CUIT']);
+            $this->load->model('padfyj_model');
+            $model_12 = 'model_12';
+            $this->load->Model($model_12);
+
+
+
+            $get_movement_data = $this->$model_12->get_order_number_print("12585/10", $list['period']);
+
+            foreach ($get_movement_data as $warranty) {
+                $cuit = $warranty[5349];
+                $brand_name = $this->padfyj_model->search_name($warranty[5349]);
+                $origen = $warranty[5215];
+            }
+
+
+
+            $new_list = array();
+            $new_list['col1'] = $list['NRO_GARANTIA'];
+            $new_list['col2'] = $list['NUMERO_CUOTA_CUYO_VENC_MODIFICA'];
+            $new_list['col3'] = $brand_name;
+            $new_list['col4'] = $cuit;
+            $new_list['col5'] = mongodate_to_print($origen);
+            $new_list['col6'] = mongodate_to_print($list['FECHA_VENC_CUOTA']);
+            $new_list['col7'] = mongodate_to_print($list['FECHA_VENC_CUOTA_NUEVA']);
+            $new_list['col8'] = money_format_custom($list['MONTO_CUOTA']);
+            $new_list['col9'] = money_format_custom($list['SALDO_AL_VENCIMIENTO']);
+
+            $rtn[] = $new_list;
+        }
+        return $rtn;
+    }
+
+    function get_anexo_datax($anexo, $parameter) {
         header('Content-type: text/html; charset=UTF-8');
         $rtn = array();
         $container = 'container.sgr_anexo_' . $anexo;
@@ -164,14 +231,56 @@ class Model_122 extends CI_Model {
         foreach ($result as $list) {
             /* Vars */
             $new_list = array();
-            $new_list['NRO_GARANTIA'] = $list['NRO_GARANTIA'];
-            $new_list['NUMERO_CUOTA_CUYO_VENC_MODIFICA'] = $list['NUMERO_CUOTA_CUYO_VENC_MODIFICA'];
-            $new_list['FECHA_VENC_CUOTA'] = $list['FECHA_VENC_CUOTA'];
-            $new_list['FECHA_VENC_CUOTA_NUEVA'] = $list['FECHA_VENC_CUOTA_NUEVA'];
-            $new_list['MONTO_CUOTA'] = money_format_custom($list['MONTO_CUOTA']);
-            $new_list['SALDO_AL_VENCIMIENTO'] = money_format_custom($list['SALDO_AL_VENCIMIENTO']);
+            $new_list['col1'] = $list['NRO_GARANTIA'];
+            $new_list['col2'] = $list['NUMERO_CUOTA_CUYO_VENC_MODIFICA'];
+            $new_list['col3'] = $list['FECHA_VENC_CUOTA'];
+            $new_list['col4'] = $list['FECHA_VENC_CUOTA_NUEVA'];
+            $new_list['col5'] = money_format_custom($list['MONTO_CUOTA']);
+            $new_list['col6'] = money_format_custom($list['SALDO_AL_VENCIMIENTO']);
             $rtn[] = $new_list;
         }
+        return $rtn;
+    }
+
+    function get_anexo_data_clean($anexo, $parameter, $xls = false) {
+
+        $rtn = array();
+        $col5 = array();
+        $col6 = array();
+        $col7 = array();
+        $col8 = array();
+        $col10 = array();
+
+        $container = 'container.sgr_anexo_' . $anexo;
+        $query = array("filename" => $parameter);
+        $result = $this->mongo->sgr->$container->find($query);
+        $new_list = array();
+        foreach ($result as $list) {
+
+            $col5[] = (float) ($list['CAIDA']);
+            $col6[] = (float) ($list['RECUPERO']);
+            $col7[] = (float) ($list['INCOBRABLES_PERIODO']);
+            $col8[] = (float) ($list['GASTOS_EFECTUADOS_PERIODO']);
+            $col9[] = (float) ($list['RECUPERO_GASTOS_PERIODO']);
+            $col10[] = (float) ($list['GASTOS_INCOBRABLES_PERIODO']);
+        }
+
+
+        $new_list = array();
+
+        $new_list['col1'] = "<strong>TOTALES</strong>";
+        $new_list['col2'] = "-";
+        $new_list['col3'] = "-";
+        $new_list['col4'] = "-";
+        $new_list['col5'] = money_format_custom($list['CAIDA']);
+        $new_list['col6'] = money_format_custom(array_sum($col6));
+        $new_list['col7'] = money_format_custom(array_sum($col7));
+        $new_list['col8'] = money_format_custom(array_sum($col8));
+        $new_list['col9'] = money_format_custom(array_sum($col9));
+        $new_list['col10'] = money_format_custom(array_sum($col10));
+        $rtn[] = $new_list;
+
+
         return $rtn;
     }
 
