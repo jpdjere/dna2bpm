a5c443134fce6f6f5da7ed03f5110da670d470f8
Diego <daotero@industria.gob.ar>
2014-03-10 11:21:04 -0300

    update all

---
+++ application/modules/sgr/views/print_to_xls.php
@@ -0,0 +1,12 @@
+<?php
+$xlsFileName = str_replace(" ", "_", $fileName);
+////Generate  file
+
+header("Content-Description: File Transfer");
+header("Content-type: application/x-msexcel");
+header("Content-Type: application/force-download");
+header("Content-Disposition: attachment; filename=" . $xlsFileName);
+header("Content-Description: PHP Generated XLS Data");
+header('Content-type: text/html; charset=UTF-8');
+?>
+{show_table}
