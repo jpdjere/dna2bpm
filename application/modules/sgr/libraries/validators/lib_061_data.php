fb5f582193e3c438272121109c69925b85462569
Vaio <Vaio@Vaio-PC>
2014-03-14 14:34:50 -0300

    Support #10141 ANEXO 06.1 - VALIDADRO A.1 - TEXTO

--- application/modules/sgr/libraries/validators/lib_061_data.php
+++ application/modules/sgr/libraries/validators/lib_061_data.php
@@ -332,7 +332,7 @@ class Lib_061_data extends MX_Controller {
          
         if ($register_on_06 != $count_on_061) {
             $code_error = ($register_on_06 > $count_on_061) ? "VG.4" : "VG.3";
-            $code_error_legend = ($register_on_06 > $count_on_061) ? "Hay CUIT que no fueron incorporados en el período" : "Hay CUIT que fueron incorporados pero no estan en el Anexo 06";
+            $code_error_legend = ($register_on_06 > $count_on_061) ? "Hay CUIT que no fueron incorporados en el período" : "Hay CUIT que no fueron incorporados en el Anexo 06";
             $stack = array();
             $result["error_row"] = 1;
             $result = return_error_array($code_error, " - ",$code_error_legend);
