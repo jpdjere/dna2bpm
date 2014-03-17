1ac5290c477a23219ac2eeb75382899c1dafa2ef
Diego <daotero@industria.gob.ar>
2014-03-17 18:02:38 -0300

    update db

--- application/modules/sgr/models/mysql_model.php
+++ application/modules/sgr/models/mysql_model.php
@@ -33,31 +33,27 @@ class mysql_model extends CI_Model {
         $container = 'container.anexo_06_' . $token . '_tmp';
         $delete = $this->mongo->sgr->$container->remove();
     }
-    
-    
+
     /* ACTIVE PERIODS DNA2 */
 
     function active_periods_dna2($anexo, $period) {
-
 
         var_dump($this->sgr_id, $this->idu);
         $this->clear_tmp();
 
         /* TRANSLATE ANEXO NAME */
         $anexo = translate_anexos_dna2($anexo);
-        
-        var_dump($anexo);
+
+
 
         $this->db->where('estado', 'activo');
         $this->db->where('anexo', $anexo);
         $this->db->where('sgr_id', $this->sgr_id);
         $query = $this->db->get('forms2.sgr_control_periodos');
 
-        
+
         $parameter = array();
         foreach ($query->result() as $row) {
-            var_dump($row);
-            
             $parameter[] = $row;
         }
 
@@ -67,13 +63,41 @@ class mysql_model extends CI_Model {
             /* ANEXO DATA */
             $this->anexo_data_tmp($anexo, $each->archivo);
         }
+
+
+        /* UPDATE MONGO BY MONGO */
+        $mongo_periods = $this->sgr_model->get_active($anexo);
+        foreach ($mongo_periods as $each) {
+           
+            
+           // $this->save_tmp($each, true);            
+            //$this->anexo_data_tmp($anexo, $each->archivo);
+        }
     }
+
+    /* SAVE FETCHS ANEXO  DATA */
 
     function anexo_data_tmp($anexo, $filename) {
 
         switch ($anexo) {
             case "sgr_socios":
                 $anexo_field = "save_anexo_06_tmp";
+
+                $this->db->select(
+                        'cuit, 
+                        tipo_socio, 
+                        tipo_operacion,
+                        cedente_cuit,
+                        codigo_actividad,
+                        cantidad_empleados, 
+                        monto,
+                        monto2,
+                        monto3, 
+                        capital_suscripto,
+                        capital_integrado,
+                        fecha_efectiva'
+                );
+
                 break;
         }
 
@@ -81,20 +105,7 @@ class mysql_model extends CI_Model {
         if ($filename != 'Sin Movimiento')
             $this->db->where('filename', $filename);
 
-        $this->db->select(
-                'cuit, 
-                tipo_socio, 
-                tipo_operacion,
-                cedente_cuit,
-                codigo_actividad,
-                cantidad_empleados, 
-                monto,
-                monto2,
-                monto3, 
-                capital_suscripto,
-                capital_integrado,
-                fecha_efectiva'
-        );
+
         $this->db->where('idu', $this->idu);
         $query = $this->db->get($anexo);
         $parameter = array();
@@ -107,8 +118,8 @@ class mysql_model extends CI_Model {
         }
     }
 
-    
-    /*SAVE FETCHS ANEXO 06 DATA*/
+    /* SAVE FETCHS ANEXO 06 DATA */
+
     function save_anexo_06_tmp($parameter) {
         $parameter = (array) $parameter;
         $token = $this->idu;
@@ -147,7 +158,7 @@ class mysql_model extends CI_Model {
 
         $parameter['FECHA_DE_TRANSACCION'] = translate_dna2_period_date($parameter['fecha_efectiva']);
 
-        
+
         unset($parameter['capital_suscripto']);
         unset($parameter['capital_suscripto']);
         unset($parameter['cantidad_empleados']);
@@ -167,29 +178,38 @@ class mysql_model extends CI_Model {
         return $out;
     }
 
-    
-    /*SAVE FETCHS PERIODOS*/
-    function save_tmp($parameter) {
+    /* SAVE FETCHS PERIODOS */
+
+    function save_tmp($parameter, $mongo = false) {
+        
+        
+         
+        
         $parameter = (array) $parameter;
         $token = $this->idu;
         $period = $this->session->userdata['period'];
         $container = 'container.periodos_' . $token . '_tmp';
 
         /* TRANSLATE ANEXO NAME */
-        $parameter['anexo'] = translate_anexos_dna2($parameter['anexo']);
-        $parameter['filename'] = $parameter['archivo'];
-
-        $parameter['period_date'] = translate_dna2_period_date($parameter['periodo']);
-
+        if (!$mongo) {
+            $parameter['anexo'] = translate_anexos_dna2($parameter['anexo']);
+            $parameter['filename'] = $parameter['archivo'];
+            $parameter['period_date'] = translate_dna2_period_date($parameter['periodo']);
+        }
+        
         $id = $this->app->genid_sgr($container);
+        
 
         $result = $this->app->put_array_sgr($id, $container, $parameter);
+        
 
         if ($result) {
             $out = array('status' => 'ok');
         } else {
             $out = array('status' => 'error');
         }
+        
+        
         return $out;
     }
 
