e5c8225294223a138f7b5fb5f9fe44426a75156a
gabriel <gabriel@gabriel-VGN-N350FE>
2014-03-20 13:10:14 -0300

    Cambio en el checkeo de headers

--- application/modules/sgr/libraries/validators/lib_16_header.php
+++ application/modules/sgr/libraries/validators/lib_16_header.php
@@ -17,7 +17,12 @@ class Lib_16_header {
             "SALDO_PROMEDIO_FDR_CONTINGENTE"
         );
         
-        $this->result = array_diff_assoc($this->headerArr, array_change_key_case($parameter, CASE_UPPER));
+        $xlsheader=array_change_key_case($parameter, CASE_UPPER);
+        $this->result = array_diff_assoc( $this->headerArr, $xlsheader);
+        foreach($this->result as $k=>$v){
+            $ingresado=(empty($xlsheader[$k]))?('Campo vacio'):($xlsheader[$k]);
+            $this->result[$k]="Valor correcto: $v -> Valor ingresado: [$ingresado]";
+        }
         return $this->result;
     }
 
