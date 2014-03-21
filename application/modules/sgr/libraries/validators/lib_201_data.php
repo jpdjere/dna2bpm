6bcac8b12331d70005232dfc9ff0a43200a75684
Diego <daotero@industria.gob.ar>
2014-03-21 12:54:52 -0300

    Support #9476 ANEXO 20.1 - VALIDADOR C.3

--- application/modules/sgr/libraries/validators/lib_201_data.php
+++ application/modules/sgr/libraries/validators/lib_201_data.php
@@ -185,7 +185,7 @@ class Lib_201_data extends MX_Controller {
                         }
 
                         /* C.3 */
-                        $lte_date = new MongoDate(strtotime(translate_for_mongo(($B_cell_value + 1))));
+                        $lte_date = new MongoDate(strtotime(translate_for_mongo(($B_cell_value))));
                         $balance = $this->$model_06->shares_active_left_until_date($C_cell_value, $lte_date);
 
                         if ($balance == 0) {
