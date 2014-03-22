5dd77ad7b964eeeae07b50ccf3d91ace1fcfb57f
Vaio <Vaio@Vaio-PC>
2014-03-22 10:57:15 -0300

    update db

--- application/modules/sgr/controllers/mysql.php
+++ application/modules/sgr/controllers/mysql.php
@@ -21,7 +21,7 @@ class Mysql extends MX_Controller {
         $this->load->model('bpm/bpm');
         $this->load->model('user/rbac');
         $this->load->model('sgr/sgr_model');
-        $this->load->model('sgr/mysql_model');
+        
         $this->load->helper('sgr/tools');
         $this->load->library('session');
 
