64c71d134312d3b3fe481ebaa21fbefbe7a8c12c
Vaio <Vaio@Vaio-PC>
2014-03-22 11:27:45 -0300

    update db

--- application/modules/sgr/models/sgr_model.php
+++ application/modules/sgr/models/sgr_model.php
@@ -133,6 +133,8 @@ class Sgr_model extends CI_Model {
     }
     
     function get_sgr_custom($idu) {
+        
+        
         $rtn = array();
         $idu = (int) $idu;
         $data = array();
@@ -141,6 +143,8 @@ class Sgr_model extends CI_Model {
         $fields = array('id', '1695', '4651', '1693', '1703');
         $query = array("owner" => $idu, "6026" => '30', "status" => 'activa');
         $result = $this->mongo->db->$container->find($query, $fields);
+        
+        var_dump($result);
 
         foreach ($result as $empresa) {
             unset($empresa['_id']);
