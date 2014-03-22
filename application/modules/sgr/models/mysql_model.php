23fc25cecdc5ac03dc743205b246642f02479a3d
Vaio <Vaio@Vaio-PC>
2014-03-22 14:05:15 -0300

    update db

--- application/modules/sgr/models/mysql_model.php
+++ application/modules/sgr/models/mysql_model.php
@@ -161,7 +161,7 @@ class mysql_model extends CI_Model {
                 $data['fecha'] = "2011-01-01 01:01:01";
                
 
-               $result = $this->db->insert('forms2.sgr_control_periodos_TEST', $data);
+               $result = $this->db->insert('forms2.sgr_control_periodos', $data);
                var_dump($result);
             }
         }
