2faeb6bfcf0f19c142d9aa155b5167bf4ecb7022
Vaio <Vaio@Vaio-PC>
2014-03-22 23:58:02 -0300

    update db

--- application/modules/sgr/libraries/validators/lib_06_data.php
+++ application/modules/sgr/libraries/validators/lib_06_data.php
@@ -1401,7 +1401,7 @@ class Lib_06_data extends MX_Controller {
             $result = return_error_array($code_error, "-", "");
             array_push($stack, $result);
         }
-        
+        debug($stack);
         exit();
         $this->data = $stack;
     }
