e19294b708792fab47f4e1e5c104723a3e376a88
Vaio <Vaio@Vaio-PC>
2014-03-28 23:28:40 -0300

    Support #10729 ANEXO 06 - PRINT - VALIDADOR 41 Y 42

--- application/modules/sgr/models/model_06.php
+++ application/modules/sgr/models/model_06.php
@@ -780,7 +780,7 @@ class Model_06 extends CI_Model {
     function shares_print($cuit, $partner_type = null, $field = 5597, $period_value, $transaction_date) {
         $anexo = $this->anexo;
         $container = 'container.sgr_anexo_' . $anexo;
-        $endDate = new MongoDate(strtotime("2013-12-31"));
+        $endDate = new MongoDate(strtotime("2014-01-01"));
 
         $buy_result_arr = array();
         $sell_result_arr = array();
