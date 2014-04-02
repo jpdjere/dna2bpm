018843ef42f6fffcc4c795429c9381483f313db8
Vaio <Vaio@Vaio-PC>
2014-04-02 12:55:21 -0300

    layout layout

--- application/modules/sgr/views/layout.php
+++ application/modules/sgr/views/layout.php
@@ -7,15 +7,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="{base_url}jscript/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" href="{base_url}jscript/bootstrap/css/bootstrap-responsive.min.css" />
-        <!--<link rel="stylesheet" href="{base_url}jscript/fontawesome/css/font-awesome.min.css" />-->
+       
         <link rel="stylesheet" href="{module_url}assets/css/font-awesome-4.0.3/css/font-awesome.min.css" />
         <link rel="stylesheet" href="{module_url}assets/jscript/jquery-ui-1.10.2.custom/css/smoothness/jquery-ui-1.10.2.custom.min.css" />	
         <link rel="stylesheet" href="{module_url}assets/css/extra-icons.css" />	
         <link rel="stylesheet" href="{module_url}assets/jscript/fullcalendar/fullcalendar.css" />
         <link rel="stylesheet" href="{module_url}assets/jscript/datepicker/css/datepicker.css" />
         <link rel="stylesheet" href="{module_url}assets/css/sgr.css" />
-        <!--/ Custom CSS -->
-        {css}
+       
 
     </head>
     <body>
@@ -33,20 +32,6 @@
         <script type="text/javascript">
             //-----declare global vars
             var globals = {inline_js};
-
-            // La clase offline es agregada por el fallback del manifiesto
-            var offline = $('.offline').length;
-
-            $(document).ready(function() {
-                if (offline) {
-                    $('#status').css('color', '#f00');
-
-                } else {
-                    $('#status').css('color', '#059B28');
-                }
-            });
-
-
 
         </script>
         {js}
