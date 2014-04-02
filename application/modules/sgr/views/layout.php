d57627e6ab39b56a985ec9affbf6342523f6e449
Vaio <Vaio@Vaio-PC>
2014-04-02 00:08:52 -0300

    layout

--- application/modules/sgr/views/layout.php
+++ application/modules/sgr/views/layout.php
@@ -16,45 +16,60 @@
         <link rel="stylesheet" href="{module_url}assets/css/sgr.css" />
         <!--/ Custom CSS -->
         {css}
-        
+
     </head>
     <body>
+        <div class="row-fluid test" id="barra_user" > 
+            <ul class="breadcrumb" style="margin-bottom:0px;padding-bottom:0px" >
+                 <li class="pull-right perfil"><a  href="{base_url}user/logout">
+                         SALIR</a></li>
+                <li class="pull-right perfil">
+                  <i class="{rol_icono}"></i> <strong> {sgr_nombre} </strong> <span class="">  {username}</span> |
+                </li>        
+                <!--<li class="pull-right perfil"><a  href="../dna2/" target="_blank"><i class="fa fa-link"></i> Acceso Versión Anterior | </a></li>-->
+               
+            </ul>
+        </div>
+        
+        <div id="header">
+            <div id="header-dna"></div>
+            <div id="header-logos"></div>
+        </div>
+        
+        
         <!--/ NAVIGATION -->
-        <div class="navbar navbar-inverse navbar-static-top ">
-            <div class="navbar-inner barra_{rol}">
+        <div class="navbar">
+            <div class="navbar-inner barra_sgr">
                 <div class="container">
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    
                     <a class="brand" href="{module_url}">SOCIEDADES DE GARANTIAS RECIPROCAS</a>
+                   
 
                     <div class="nav-collapse collapse">
-                        <ul class="nav">
-                        </ul>
-                        <ul class="nav pull-right inline"> 
-                            
+                        <ul class="nav pull-right inline">
                             {if sgr_period}
                             <li><a href="{base_url}sgr/unset_period" id="icon-calendar"><i class="icon-calendar"></i> Período: <span id="sgr_period"> {sgr_period}</span></a></li>    
                             {/if}
                             <li class="dropdown" id="menu-messages">
                                 <a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle">
                                     <i class="fa fa-file-text">
-                                     </i> <span class="text"> Anexos</span> <span class="label label-important"> {anexo_title_cap} </span> <b class="caret">
+                                    </i> <span class="text"> Anexos:</span> <span class=""> {anexo_title_cap} </span> <b class="caret">
                                     </b>
                                 </a>
                                 <ul class="dropdown-menu">
-                                   {anexo_list}
+                                    {anexo_list}
                                 </ul>
                             </li>
-                            <li><a  href="{base_url}user/logout"><i class="fa fa-power-off"></i> Salir</a></li>                            
+                            <li></li>                            
                         </ul>
                     </div>
                 </div>
+
+
+
             </div>
         </div>
-
+        
         <!-- CONTAINER -->
         {content}
         <!-- CONTAINER -->
