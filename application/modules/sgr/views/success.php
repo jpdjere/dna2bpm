e660ca89f397c96229869c1e140e477f93c08b68
Vaio <Vaio@Vaio-PC>
2014-04-02 11:34:37 -0300

    layout success

--- application/modules/sgr/views/success.php
+++ application/modules/sgr/views/success.php
@@ -1,16 +1,27 @@
-<div class="row-fluid test" id="barra_user" > 
-    <ul class="breadcrumb" style="margin-bottom:0px;padding-bottom:0px" >
-        <i class="fa fa-backward"></i><a href="../../sgr/"> Volver</a> | <i class="fa fa-print"></i> <a href="javascript:window.print()">Imprimir pantalla</a>
-            
-        <li class="pull-right perfil">
-            SGR: {sgr_nombre}  <span id="status"> <i class="{rol_icono}"></i> {username} [Grupo: {rol}]</span>
-        </li>        
-        <li class="pull-right perfil"><a  href="../dna2/" target="_blank"><i class="fa fa-link"></i> Versión Anterior | </a></li>
-    </ul>
-</div>
+
 <!-- ==== Contenido ==== -->
 <div class="container" > 
-    <h1 class="success">Información sobre la importación del Anexo</h1>
+    
+    <div class="container" > 
+    <div class="row-fluid test" id="barra_user" > 
+        <ul class="breadcrumb" style="margin-bottom:0px;padding-bottom:0px" >
+            <li class="pull-right perfil"><a  href="{base_url}user/logout">
+                    SALIR</a></li>
+            <li class="pull-right perfil">
+                <i class="{rol_icono}"></i> <strong> {sgr_nombre} </strong> <span class="">  {username}</span> |
+            </li>        
+            <!--<li class="pull-right perfil"><a  href="../dna2/" target="_blank"><i class="fa fa-link"></i> Acceso Versión Anterior | </a></li>-->
+
+        </ul>
+    </div>
+
+    <div id="header">
+        <div id="header-dna"></div>
+        <div id="header-logos"></div>
+    </div>
+
+     <h2><i class="fa fa-bars"></i> Información sobre la importación de {anexo_title_cap}</h2>
+
     <div class="alert alert-success" id="{_id}">       
             
         <h2>El Anexo se proceso correctamente.</h2>
