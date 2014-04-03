476cc78694a9151b7b6bd082bf30ccf7dbddef0e
Vaio <Vaio@Vaio-PC>
2014-04-03 19:35:26 -0300

    Support #10981 Anexo 20.2 - Prueba Acindar V-A.3

--- application/modules/sgr/libraries/validators/lib_202_data.php
+++ application/modules/sgr/libraries/validators/lib_202_data.php
@@ -183,9 +183,7 @@ class Lib_202_data extends MX_Controller {
                 array_push($stack, $result);
             }
         }
-//
-        var_dump($stack);
-        exit();
+//        var_dump($stack);        exit();
         $this->data = $stack;
     }
 
