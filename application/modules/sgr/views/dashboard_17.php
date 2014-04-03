f1e5e31aaf468705a321aad2034c7739c9b4e464
Vaio <Vaio@Vaio-PC>
2014-04-03 18:17:47 -0300

    Support #10984 Anexo 17 - Navegación

--- application/modules/sgr/views/dashboard_17.php
+++ application/modules/sgr/views/dashboard_17.php
@@ -51,73 +51,11 @@
 
         </div>
     </div>
-
-
+ 
     <h2><i class="fa fa-bars"></i> {anexo_short} {anexo_title_cap}</h2>  
-
-    <div class="row-fluid test" id="barra_user" > 
-        <ul class="breadcrumb" style="margin-bottom:0px;padding-bottom:0px" >
-
-            <button type="button" class="btn hide_offline" data-toggle="collapse" data-target="#file_div">
-                <i class="fa fa-plus"></i>  Seleccionar Archivos a Procesar
-            </button> 
-            {if sgr_period}        
-            <button type="button" id="no_movement" class="no_movement btn btn-info" value="{sgr_period}">
-                 Asociar el periodo {sgr_period} a "Sin Movimientos"
-            </button>
-            {/if}        
-
-        </ul>
-    </div>
-    <!-- MESSAGES -->
-    {if period_message}
-    <div class="alert alert-{success}" id="{success}">   
-        {period_message}
-    </div>
-    {else}
-    {if message}
-    <div class="alert alert-{success}" id="{success}">   
-        {message}
-    </div>
-    {/if}
-    {/if}
-
-    <!-- RECTIFICATION ALERT-->
-    {rectify_message_template}
-
-
-
-    <div class="row-fluid">
-        {if $files_list}
-        {else}
-        <!-- FILE UPLOAD -->
-        {upload_form_template}
-        {/if}
+    <div class="row-fluid">       
     </div>
 
-
-    <!-- FORM PERIOD--> 
-    {form_template}
-    <!-- UPLOADED FILES LIST-->
-    {files_list}   
-
-    <!-- PENDING LIST -->
-    {if pending_list}
-    <div class="well">
-        <!-- PENDING ANEXOS-->
-        <div id="show_anexos" class="alert alert-error">
-
-            <ul>
-                {pending_list}
-                <li>Para finalizar deberá importar el ANEXO 6.1 – RELACIONES DE VINCULACIÓN. De no cargarse, se cancelará toda la importación del ANEXO 6 – MOVIMIENTOS DE CAPITAL SOCIAL.</li>
-                <li><a href="sgr/anexo_code/061">Continuar</a></li>
-            </ul>
-
-        </div>
-
-    </div>
-    <hr>
-    {/if}
 
     {if processed_list}
     <div class="well">
