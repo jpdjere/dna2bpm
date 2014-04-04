6fc9185635a22bfb1a3d8fe709230af6b4fd01e6
Diego <daotero@industria.gob.ar>
2014-04-04 10:59:56 -0300

    Support #10595 ANEXO 09 - BALANCES EN PDF

---
+++ application/modules/sgr/models/mysql_model_09.php
@@ -0,0 +1,166 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class mysql_model_09 extends CI_Model {
+
+    function mysql_model_09() {
+        parent::__construct();
+        // IDU : Chequeo de sesion
+        $this->idu = (float) $this->session->userdata('iduser');
+        if (!$this->idu) {
+            header("$this->module_url/user/logout");
+            exit();
+        }
+
+        /* DATOS SGR */
+        $sgrArr = $this->sgr_model->get_sgr();
+        foreach ($sgrArr as $sgr) {
+            $this->sgr_id = $sgr['id'];
+            $this->sgr_nombre = $sgr['1693'];
+            $this->sgr_cuit = $sgr['1695'];
+        }
+
+        $dbconnect = $this->load->database('dna2');
+    }
+
+    /* ACTIVE PERIODS DNA2 */
+
+    function active_periods_dna2($anexo, $period) {
+
+
+
+        /* TRANSLATE ANEXO NAME */
+        $anexo_dna2 = translate_anexos_dna2($anexo);
+        $this->db->where('estado', 'activo');
+        $this->db->where('archivo !=', 'Sin Movimiento');
+        $this->db->where('anexo', $anexo_dna2);
+        $query = $this->db->get('forms2.sgr_control_periodos');
+
+
+        foreach ($query->result() as $row) {
+
+
+
+            $already_period = $this->already_period($row->archivo);
+            if (!$already_period) {
+
+                $parameter = array();
+
+                $parameter['anexo'] = translate_anexos_dna2($row->anexo);
+                $parameter['filename'] = $row->archivo;
+                $parameter['period_date'] = translate_dna2_period_date($row->periodo);
+                $parameter['sgr_id'] = (float) $row->sgr_id;
+                $parameter['status'] = 'activo';
+                $parameter['origen'] = 'forms2';
+                $parameter['period'] = str_replace("_", "-", $row->periodo);
+
+
+                /* UPDATE CTRL PERIOD */
+                $this->save_tmp($parameter);
+
+                /* UPDATE ANEXO */
+                if ($row->archivo) {
+                    $already_update = $this->already_updated($row->anexo, $nro_orden, $filename);
+                    if (!$already_update)
+                        $this->anexo_data_tmp($anexo_dna2, $row->archivo);
+                }
+            }
+        }
+    }
+
+    function save_tmp($parameter) {
+
+
+        $parameter = (array) $parameter;
+        $container = 'container.sgr_periodos';
+
+        $id = $this->app->genid_sgr($container);
+        $result = $this->app->put_array_sgr($id, $container, $parameter);
+        if ($result) {
+            $out = array('status' => 'ok');
+        } else {
+            $out = array('status' => 'error');
+        }
+        return $out;
+    }
+
+    /* SAVE FETCHS ANEXO  DATA */
+
+    function anexo_data_tmp($anexo, $filename) {
+
+
+
+
+        $this->db->select(
+                'id,
+                filename, cuit_sgr
+                idu'
+        );
+
+        if ($filename != 'Sin Movimiento')
+            $this->db->where('filename', $filename);
+
+
+
+        $query = $this->db->get($anexo);
+        $parameter = array();
+        foreach ($query->result() as $row) {
+
+            $parameter = array();
+
+            /* STRING */
+            $parameter['CUIT_SGR'] = (string) $row->cuit_sgr;
+            
+
+            $parameter['idu'] = (float) $row->idu;
+            $parameter['filename'] = (string) $row->filename;
+            $parameter['id'] = (float) $row->id;
+            $parameter['origen'] = 'forms2';
+
+            debug($parameter);
+
+            $this->save_anexo_09_tmp($parameter, $anexo);
+        }
+    }
+
+    /* SAVE FETCHS ANEXO 09 DATA */
+
+    function already_period($filename) {
+
+        $container = 'container.sgr_periodos';
+        $query = array("filename" => $filename);
+        $result = $this->mongo->sgr->$container->findOne($query);
+        if ($result)
+            return true;
+    }
+
+    function already_updated($anexo, $nro_orden, $filename) {
+
+        $container = 'container.sgr_anexo_' . $anexo;
+        $query = array("filename" => $filename);
+        $result = $this->mongo->sgr->$container->findOne($query);
+
+        if ($result)
+            return true;
+    }
+
+    function save_anexo_09_tmp($parameter, $anexo) {
+        $parameter = (array) $parameter;
+        $token = $this->idu;
+        $period = $this->session->userdata['period'];
+        $container = 'container.sgr_anexo_09';
+        /* TRANSLATE ANEXO NAME */
+
+        $id = $this->app->genid_sgr($container);
+        $result = $this->app->put_array_sgr($id, $container, $parameter);
+        if ($result) {
+            $out = array('status' => 'ok');
+        } else {
+            $out = array('status' => 'error');
+        }
+        return $out;
+    }
+
+}
