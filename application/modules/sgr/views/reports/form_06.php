456513f4d2f514c328f2fe191eeffdf06aa29dd1
Vaio <Vaio@Vaio-PC>
2014-04-05 17:26:29 -0300

    reports

--- application/modules/sgr/views/reports/form_06.php
+++ application/modules/sgr/views/reports/form_06.php
@@ -1,21 +1,22 @@
 
 <div class="row-fluid">
     <div id="meta_div_2">
-        <form  method="post" class="well" id="period_{sgr_period}" >
+        <form  method="post" class="well" id="form">
             <div  class="row-fluid " >
                 <div class="span6">                        
-                   <div>
-                            <label>Seleccione Desde </label>
-                            <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date dp">
-                                <input type="text" name="input_period_from"  class="input-block-level">                                       
-                                       <span class="add-on"><i class="icon-calendar"></i></span>                                
-                            </div>
+                    <div>
+                        <label>Seleccione Desde </label>
+                        <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date dp">
+                            <input type="text" name="input_period_from"  class="input-block-level">                                       
+                            <span class="add-on"><i class="icon-calendar"></i></span>                                
                         </div>
+                    </div>
 
                     <div>
                         <label>Seleccione la SGR</label>
-                        <select name="rectify" id="rectify_{sgr_period}" class="input-block-level required">
-                            {sgr_options}
+                        <select name="sgr" id="sgr" class="input-block-level required">
+                            <!-- {sgr_options}-->
+                            <option value="2478671474">ACINDAR PYMES S.G.R.</option>
                         </select>
                     </div>                       
 
@@ -23,20 +24,20 @@
 
                 <div class="span6">
                     <div>
-                            <label>Seleccione Hasta </label>
-                            <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date dp">
-                                <input type="text" name="input_period_to" class="input-block-level">                                       
-                                       <span class="add-on"><i class="icon-calendar"></i></span>                                
-                            </div>
+                        <label>Seleccione Hasta </label>
+                        <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date dp">
+                            <input type="text" name="input_period_to" class="input-block-level">                                       
+                            <span class="add-on"><i class="icon-calendar"></i></span>                                
                         </div>
+                    </div>
 
                 </div>
             </div>
             <div  class="row-fluid">
                 <div class="span12">
-                        <input type="hidden" name="anexo" value="{anexo}" />
-                        <button name="submit_period" class="btn btn-block btn-primary hide_offline" type="submit" id="bt_save_{sgr_period}"><i class="fa fa-search"></i> Buscar</button>  
-                    </div>
+                    <input type="hidden" name="anexo" value="{anexo}" />
+                    <button name="submit_period" class="btn btn-block btn-primary hide_offline" type="submit" id="bt_save_{sgr_period}"><i class="fa fa-search"></i> Buscar</button>  
+                </div>
             </div>
         </form>
     </div>
