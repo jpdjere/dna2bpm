e4016127357b1bcd419c620417723ca6c1319a18
Vaio <Vaio@Vaio-PC>
2014-04-06 12:46:33 -0300

    Support #11042 Anexo 14 - Nuevo Validador C.2.B

--- application/modules/sgr/libraries/validators/lib_14_data.php
+++ application/modules/sgr/libraries/validators/lib_14_data.php
@@ -508,7 +508,7 @@ class Lib_14_data extends MX_Controller {
                 }
             }
         }
-        //var_dump($stack);      exit();                    
+        var_dump($stack);      exit();                    
         $this->data = $stack;
     }
 
