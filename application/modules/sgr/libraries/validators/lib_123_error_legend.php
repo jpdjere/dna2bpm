b68b41c57247f1f08f24e22d66e6135ec14f6439
Vaio <Vaio@Vaio-PC>
2014-04-06 14:58:56 -0300

    Support #11042 Anexo 14 - Nuevo Validador C.2.B

--- application/modules/sgr/libraries/validators/lib_123_error_legend.php
+++ application/modules/sgr/libraries/validators/lib_123_error_legend.php
@@ -12,9 +12,13 @@ class Lib_123_error_legend {
             case "A.1":
                 $result_error = '<strong>Columna A - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>El Número de garantía debe estar informado en el sistema y corresponder con alguno de los siguientes tipos de garantías:GFMFO/GC1/GC2/GT';
                 break;
-            case "B.1":
+            case "B.1.A":
                 $result_error = '<strong>Columna B - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Los montos informados deben ser menores o iguales al Monto de Garantía Otorgado registrado en el Sistema.';
                 break;
+            case "B.1.B":
+                $result_error = '<strong>Columna B - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>GARANTÍAS EN DÓLARES: los montos informados deben ser inferiores o iguales al monto en pesos informado para esa misma garantía en la columna E del anexo 12, dividido el Tipo de Cambio de la fecha de otorgamiento, multiplicado por el Tipo de Cambio vigente al último día del período que se está informando.';
+                break;
+            
             case "B.2":
                 $result_error = '<strong>Columna B - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Si algún día el saldo estuvo en Cero, deben informar “0”. Ningún campo puede estar vacío.';
                 break;
