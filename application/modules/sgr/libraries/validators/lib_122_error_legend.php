dfe93d7f558e29e92c8012751dc041c58879e8ac
Vaio <Vaio@Vaio-PC>
2014-04-06 22:23:44 -0300

    Support #11042 Anexo 14 - Nuevo Validador C.2.B

--- application/modules/sgr/libraries/validators/lib_122_error_legend.php
+++ application/modules/sgr/libraries/validators/lib_122_error_legend.php
@@ -19,7 +19,7 @@ class Lib_122_error_legend {
                 $result_error = '<strong>Columna B - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Una misma cuota de una garantía no puede figurar dos veces en el archivo importado.';
                 break;
              case "C.1": 
-                $result_error = '<strong>Columna C - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Formato numérico, entero, mayor que cero.Formato numérico de cinco dígitos sin decimales. ';
+                $result_error = '<strong>Columna C - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Formato numérico de cinco dígitos sin decimales.';
                 break;
              case "C.2": 
                 $result_error = '<strong>Columna C - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Fecha debe ser posterior a la fecha de emisión de la garantía indicada en la columna “A”.';
@@ -33,7 +33,10 @@ class Lib_122_error_legend {
             case "F.1": 
                 $result_error = '<strong>Columna F - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Formato numérico. Aceptar hasta dos decimales. ';
                 break;
-            case "F.2": 
+            case "F.2.A": 
+                $result_error = '<strong>Columna F - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>El monto debe ser inferior al registrado en el sistema como “Monto de la Garantía” para la indicada en la columna “A”.';
+                break;
+             case "F.2.B": 
                 $result_error = '<strong>Columna F - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>El monto debe ser inferior al registrado en el sistema como “Monto de la Garantía” para la indicada en la columna “A”.';
                 break;
         }
