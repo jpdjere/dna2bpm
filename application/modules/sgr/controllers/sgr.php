1df0a66169a18feae9223c33ed5e5b05073f6b57
Vaio <Vaio@Vaio-PC>
2014-04-10 17:08:22 -0300

    PDF

--- application/modules/sgr/controllers/sgr.php
+++ application/modules/sgr/controllers/sgr.php
@@ -1041,8 +1041,9 @@ class Sgr extends MX_Controller {
                     $print_filename = $file['filename'];
 
                     $show_period = ($i != 2010) ? $file['period'] : "ADMINISTRADOR";
+                    $asset = ($anexo=="09")? "pdf_asset":"xls_asset";
 
-                    $download = anchor('sgr/xls_asset/' . $anexo . '/' . $file['filename'], ' <i class="fa fa-download" alt="Descargar"></i>', array('class' => 'btn btn-primary' . $disabled_link));
+                    $download = anchor('sgr/'.$asset.'/' . $anexo . '/' . $file['filename'], ' <i class="fa fa-download" alt="Descargar"></i>', array('class' => 'btn btn-primary' . $disabled_link));
                     $print_file = anchor('sgr/dna2_asset/XML-Import/' . translate_anexos_dna2_urls($anexo) . '/' . $file['filename'], ' <i class="fa fa-print" alt="Imprimir"></i>', array('target' => '_blank', 'class' => 'btn btn-primary'));
 
                     $print_xls_link = anchor('/sgr/print_xls/' . $file['filename'], ' <i class="fa fa-table" alt="XLS"></i>', array('target' => '_blank', 'class' => 'btn btn-primary' . $disabled_link));
