c2c2a294faffef3205675eeedf6aa169cd0ba250
Diego <daotero@industria.gob.ar>
2014-04-16 16:00:52 -0300

    Support #11426 ANEXO 20.2 - VALIDADOR A.2

--- application/modules/sgr/libraries/validators/lib_202_data.php
+++ application/modules/sgr/libraries/validators/lib_202_data.php
@@ -156,7 +156,7 @@ class Lib_202_data extends MX_Controller {
                         if ($nro_aporte) {
                             /* ESTA EN EL SISTEMA */
                             
-                            var_dump($nro_aporte, $get_input_number_check, $C_cell_value,$D_cell_value);
+                            echo $C_cell_value."->".$D_cell_value;
                             
                             if ($get_input_number_check == 0 && ($C_cell_value != 0 || !$D_cell_value)) {
                                 
