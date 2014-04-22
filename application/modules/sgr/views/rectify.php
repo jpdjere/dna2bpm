05af4a7632b273d34f4ac0c0ac5ccc4ba12c2f48
Gabriel Fojo <gabriel@trialvd.com.ar>
2014-04-22 12:33:20 -0300

    arreglo en modal rectificacion

--- application/modules/sgr/views/rectify.php
+++ application/modules/sgr/views/rectify.php
@@ -5,12 +5,16 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h3 class="modal-title" id="myModalLabel">Rectificación Período {rectify_message}</h3>
             </div>
+           <div class="modal-body">
             <div class="alert alert-danger">                
-                <div class="navbar-inverse well-small"><i class="fa fa-info-circle"></i> Para continuar con la rectificación del Anexo seleccionado deberá asociar el perido {rectify_message} a un nuevo Archivo o a la opción "SIN MOVIMIENTO."</div>
+                <div well-small"><i class="fa fa-info-circle"></i> Para continuar con la rectificación del Anexo seleccionado deberá asociar el perido {rectify_message} a un nuevo Archivo o a la opción "SIN MOVIMIENTO2."</div>
             </div>
-            <div class="alert">
+            <div class="alert alert-warning">
                 Al seleccionar un nuevo periódo se cancelará la operación.                
             </div>
+      </div>
+      
+
             <div class="modal-footer">        
                 <button type="button" class="btn btn-primary" data-dismiss="modal">Continuar</button>                 
             </div>
