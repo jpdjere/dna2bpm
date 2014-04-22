c544f229e94e9bac747f382daf83f9c2ae60a71d
Gabriel Fojo <gabriel@trialvd.com.ar>
2014-04-22 14:56:49 -0300

    Modificacion vews reportes para Bootstrap3

--- application/modules/sgr/views/reports/form_06.php
+++ application/modules/sgr/views/reports/form_06.php
@@ -1,54 +1,77 @@
-<div class="row-fluid">
-    <div id="meta_div_2">
-        <form  method="post" class="well" id="form" target="_blank" action="reports/action_form/">
-            <div  class="row-fluid " >
-                <div class="span6">                        
-                    <div>
-                        <label>Seleccione Desde </label>
-                        <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date dp">
-                            <input type="text" name="input_period_from"  class="input-block-level">                                       
-                            <span class="add-on"><i class="icon-calendar"></i></span>                                
-                        </div>
-                    </div>
 
-                    <div>
-                        <label>Seleccione la SGR</label>
-                        <select name="sgr" id="sgr" class="input-block-level required" readonly>
-                            {sgr_options}
-                        </select>
-                    </div>                       
+		<form method="post" class="well" id="form" target="_blank" action="reports/action_form/">
 
-                </div>
+			<div class="row ">
+				<!--  ========================== row 4  ========================== -->
+				<div class="col-md-4" >
+						<!--  Desde  -->
+						<div class="row ">
+							<div class="form-group col-md-12">
+								<label>Desde</label>
+								<div class="input-group date dp" data-date-viewMode="months"
+									data-date-minViewMode="months" data-date-format="mm-yyyy"
+									data-date="">
+									<span class="add-on input-group-addon"><i class="fa fa-calendar"></i></span>
+									<input type="text" name="input_period_from" readonly=""
+										class="form-control">
+								</div>
+							</div>
+						</div>
+						<!--  Hasta  -->
+						<div class="row ">
+							<div class="form-group col-md-12">
+								<label>Hasta</label>
+								<div class="input-group date dp" data-date-viewMode="months"
+									data-date-minViewMode="months" data-date-format="mm-yyyy"
+									data-date="">
+									<span class="add-on input-group-addon"><i class="fa fa-calendar"></i></span>
+									<input type="text" name="input_period_to" readonly="" class="form-control">
+								</div>
+							</div>
+						</div>
+				</div><!-- row4-->
+				<!--  ========================== row 8  ========================== -->
+				<div class="col-md-8" >
+				<!--  SGR -->
+					<div class="row">
+						<div class="form-group col-md-12">
+						<label>Seleccione la SGR</label> 
+						<div class="input-group ">
+						<select name="sgr" id="sgr"	class="required form-control" > {sgr_options}</select>
+						</div>	
+						</div>
+					</div>
+				<!--  Reporte  -->
+					<div class="row ">
+					<div class="form-group col-md-12">
+						<label>Reporte</label> 
+						<div class="input-group ">
+						<select name="report_name" id="sgr" class="form-control required">
+							<option value="A">A.- MOVIMIENTOS CAPITAL SOCIAL</option>
+							<option value="B" disabled="disabled">B.- CUENTA CORRIENTE
+								CAPITAL SOCIAL (No Disponible)</option>
+							<option value="C" disabled="disabled">C.- SALDOS CAPITAL SOCIAL
+								(No Disponible)</option>
+						</select>
+						</div>
+						</div>
+					</div>
+					
 
-                <div class="span6">
-                    <div>
-                        <label>Seleccione Hasta </label>
-                        <div data-date-viewMode="months" data-date-minViewMode="months" data-date-format="mm-yyyy" data-date="" id="dp3" class="input-append date dp">
-                            <input type="text" name="input_period_to" class="input-block-level">                                       
-                            <span class="add-on"><i class="icon-calendar"></i></span>                                
-                        </div>
-                    </div>
+				</div><!-- row8 -->
+			</div>
 
-                </div>
 
-                <div class="span6">
-                    <div>
-                        <label>Reporte </label>
-                        <select name="report_name" id="sgr" class="input-block-level required">
-                            <option value="A">A.- MOVIMIENTOS CAPITAL SOCIAL</option>
-                            <option value="B" disabled="disabled">B.- CUENTA CORRIENTE CAPITAL SOCIAL (No Disponible)</option>
-                            <option value="C" disabled="disabled">C.- SALDOS CAPITAL SOCIAL (No Disponible)</option>
-                        </select>
-                    </div>
 
-                </div>
-            </div>
-            <div  class="row-fluid">
-                <div class="span12">
-                    <input type="hidden" name="anexo" value="{anexo}" />
-                    <button name="submit_period" class="btn btn-block btn-primary hide_offline" type="submit" id="bt_save_{sgr_period}"><i class="fa fa-search"></i> Buscar</button>  
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
+			<!--  ROW 3  -->
+			<div class="row">
+				<div class="col-md-12">
+					<input type="hidden" name="anexo" value="{anexo}" />
+					<button name="submit_period"
+						class="btn btn-block btn-primary hide_offline" type="submit"
+						id="bt_save_{sgr_period}">
+						<i class="fa fa-search"></i> Buscar
+					</button>
+				</div>
+			</div>
+		</form>
