93b8a8bd5032c6bfc4d806c202a6a0ed71f5bbb7
Vaio <Vaio@Vaio-PC>
2014-04-26 14:19:22 -0300

    Support #11733 ANEXO 20.2 - VALIDADOR A.3

--- application/modules/sgr/models/model_201.php
+++ application/modules/sgr/models/model_201.php
@@ -466,7 +466,8 @@ class Model_201 extends CI_Model {
             $io_result = $this->mongo->sgr->$container->find($new_query);
             foreach ($io_result as $data) {
                 if ($data) {
-                    $rtn[] = ($data['NUMERO_DE_APORTE']);
+                    debug($data['NUMERO_DE_APORTE']);
+                    $rtn[] = $data['NUMERO_DE_APORTE'];
                 }
             }
 
