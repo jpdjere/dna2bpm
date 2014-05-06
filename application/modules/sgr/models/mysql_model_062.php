203cd3fdfca307805acb628f15dcd351383821df
Diego <daotero@industria.gob.ar>
2014-05-06 11:00:39 -0300

    Fix #11963 (Urg.) Importación de Anexos 2014 en Sistema Nuevo

--- application/modules/sgr/models/mysql_model_062.php
+++ application/modules/sgr/models/mysql_model_062.php
@@ -57,14 +57,18 @@ class mysql_model_062 extends CI_Model {
                 $parameter['period'] = str_replace("_", "-", $row->periodo);
 
 
-                /* UPDATE CTRL PERIOD */
-                $this->save_tmp($parameter);
+                $is_2014 = explode("_", $row->periodo);
+                if ($is_2014[1]!="2014") {
+                    
+                    /* UPDATE CTRL PERIOD */
+                    $this->save_tmp($parameter);
 
-                /* UPDATE ANEXO */
-                if ($row->archivo) {
-                    $already_update = $this->already_updated($row->anexo, $nro_orden, $filename);
-                    if (!$already_update)
-                        $this->anexo_data_tmp($anexo_dna2, $row->archivo);
+                    /* UPDATE ANEXO */
+                    if ($row->archivo) {
+                        $already_update = $this->already_updated($row->anexo, $nro_orden, $filename);
+                        if (!$already_update)
+                            $this->anexo_data_tmp($anexo_dna2, $row->archivo);
+                    }
                 }
             }
         }
