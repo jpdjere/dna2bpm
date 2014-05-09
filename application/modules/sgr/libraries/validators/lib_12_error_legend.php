c263b3d72c46f797161bc784d2d036a0b5740d27
Diego <daotero@industria.gob.ar>
2014-05-09 12:36:43 -0300

    Support #12131 Anexo 12 - validador O.1

--- application/modules/sgr/libraries/validators/lib_12_error_legend.php
+++ application/modules/sgr/libraries/validators/lib_12_error_legend.php
@@ -78,7 +78,7 @@ class Lib_12_error_legend {
                 $result_error = '<strong>Columna N - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Debe contener uno de los siguientes parámetros: "FIJA"/"LIBOR"/"BADLARPU" (Badlar Bancos Públicos)/"BADLARPR" (Badlar Bancos Privados)/"TEC"/"TEBP".';
                 break;
             case "O.1":
-                $result_error = '<strong>Columna O - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Debe tomar valores entre -20 y -1 o entre 1 y 20 (es decir, no puede tomar valores entre -1 y 1).';
+                $result_error = '<strong>Columna O - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Debe tomar valores entre -1 y -50 o entre 1 y 50 (es decir, no puede tomar valores entre -1 y 1).';
                 break;
             case "O.2":
                 $result_error = '<strong>Columna O - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Si en la Columna N se indicó que la tasa es “FIJA”, debe tomar un valor entre -25 y 50, con dos decimales.';
