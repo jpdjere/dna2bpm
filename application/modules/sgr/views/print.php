5d36758053f1cfc223a3d05df08dc4f7455c340f
Gabriel Fojo <gabriel@trialvd.com.ar>
2014-05-27 13:46:02 -0300

    pdf para todos y todas

--- application/modules/sgr/views/print.php
+++ application/modules/sgr/views/print.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>DNA&sup2; | SGR | {sgr_nombre}</title>
-        <meta charset="UTF-8" />
-        <link rel="stylesheet" href="{base_url}jscript/bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="{base_url}jscript/bootstrap/css/bootstrap-responsive.min.css" />
-        <link rel="stylesheet" href="{module_url}assets/css/font-awesome-4.0.3/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="{module_url}assets/css/sgr.css" />
-        <link rel="stylesheet" href="{module_url}assets/css/print.css" />
-    </head>
-    <body>
-        <div class="container" > 
-            <div class="navbar navbar-inverse navbar-static-top ">
-                <div id="header">
-                    <div id="header-dna"></div>
-                    <div id="header-logos"></div>
-                </div>
-                <div class="row-fluid"> 
-                    <h2><i class="fa fa-bars"></i> {sgr_nombre} | C.U.I.T.: <span class="text-info">{sgr_cuit}</span></h2>
-                    <p>[Anexo {anexo_short}] SGR {anexo_title} - Importado por: [{user_print}]<br/>
-                        <small>Archivo Procesado: {parameter}</small>                                  
-                        <span class="pull-right perfil">
-                            Información correspondiente al período {print_period} | <a href="javascript:window.print();"><i class="fa fa-print" alt="Imprimir"></i></a> | <a href='javascript:window.close();'>CERRAR</a><br>
-                        </span></p>
-                </div>
-                {show_table}
+<head>
+<title>DNA&sup2; | SGR | {sgr_nombre}</title>
+<meta charset="UTF-8" />
+<link rel="stylesheet" 	href="{module_url}jscript/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet"	href="{module_url}assets/css/font-awesome-4.0.3/css/font-awesome.min.css" />
+<link rel="stylesheet" href="{module_url}assets/css/sgr.css" />
+<!-- <link rel="stylesheet"  href="{module_url}assets/css/print.css"> --> 
+<style>
 
-            </div>
+th, td { padding: .65em; }
+th, thead { background: #fff; color: #000; border: 1px solid #777; font-weight:bold; }  
+td { border: 1px solid #777;  }
+
+table {border:1px solid #ccc;font-family: Futura, Arial, sans-serif; font-size:8px; text-align: center; width:100%;}
+</style>
+</head>
+<body>
+    <body>
+    	<div class=row" align="center">
+    	   <img src="{module_url}assets/images/orgullo.jpg" >
+	       <h2><i class="fa fa-bars"></i> {sgr_nombre} | C.U.I.T.: <span class="text-info">{sgr_cuit}</span></h2>
+           <p>[Anexo {anexo_short}] SGR {anexo_title} - Importado por: [{user_print}]</p>
+           <p><small>Archivo Procesado: {parameter}</small></p>                               
+          <p><span class="">
+              Información correspondiente al período {print_period}
+          </span></p>		
+        </div>
+
+
+		{show_table}
+
             {if show_footer}
             <ul><li>NOTA:<small>{show_footer}</small></li></ul>
             {/if}
