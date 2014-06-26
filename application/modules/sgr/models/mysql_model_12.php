b441d422b167d0280be2e95c6b6ccdadb204ca11
Diego <daotero@industria.gob.ar>
2014-06-26 13:25:50 -0300

    FIX DON MARIO ANEXO 12 - DON MARIO S.G.R. - 2014-04-08 03:36:48.xls

--- application/modules/sgr/models/mysql_model_12.php
+++ application/modules/sgr/models/mysql_model_12.php
@@ -45,7 +45,7 @@ class mysql_model_12 extends CI_Model {
         /* TRANSLATE ANEXO NAME */
         $anexo_dna2 = translate_anexos_dna2($anexo);
         $this->db->where('estado', 'activo');
-        $this->db->where('archivo', 'ANEXO 12 - DON MARIO S.G.R. - 2014-06-26 10:43:41.xls');
+        $this->db->where('archivo', 'ANEXO 12 - DON MARIO S.G.R. - 2014-04-08 03:36:48.xls');
         $this->db->where('anexo', $anexo_dna2);
        //$this->db->where('sgr_id', 2207746538);
         
