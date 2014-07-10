6ed1636e8573c07557d1d26a3bf98f2556c819b5
Diego <daotero@industria.gob.ar>
2014-07-10 11:49:46 -0300

    Feature #13122

--- application/modules/sgr/models/model_202.php
+++ application/modules/sgr/models/model_202.php
@@ -85,8 +85,6 @@ class Model_202 extends CI_Model {
             }
             
             
-            
-            
             $retiros = array_sum(array($get_movement_data['RETIRO']));
             $saldo = $get_movement_data['APORTE'] - $retiros;
             $disponible = $saldo - (float) $insertarr['CONTINGENTE_PROPORCIONAL_ASIGNADO'];
@@ -95,13 +93,7 @@ class Model_202 extends CI_Model {
             $insertarr["CUIT_PROTECTOR"] = $cuit;
             $insertarr["SALDO"] = $saldo;
             $insertarr["DISPONIBLE"] = $disponible;
-            
-            
-            if ($this->sgr_id == 2207746538) {
-                debug($this->sgr_id);
-                debug($get_movement_data);
-                exit();
-            }
+          
             
             
         }
