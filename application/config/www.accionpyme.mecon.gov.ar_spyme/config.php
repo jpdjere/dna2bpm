2bfc75e3dc7b2d7b8986b7a1ded35e13a4da7c4d
Diego <daotero@industria.gob.ar>
2014-08-04 15:26:46 -0300

    error_reporting

--- application/config/www.accionpyme.mecon.gov.ar_spyme/config.php
+++ application/config/www.accionpyme.mecon.gov.ar_spyme/config.php
@@ -1,5 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+error_reporting(0);
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
