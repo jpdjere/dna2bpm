90b7de0a96bce431198f6b2f2e2afae2ebac982b
www-data <www-data@tests.mecon.ar>
2014-08-11 11:40:20 -0300

    saco logs

--- application/logs/log-2014-07-24.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-07-24 14:01:53 --> Severity: Notice  --> Undefined variable: test /var/www/dna2bpm/application/models/msg.php 42
-ERROR - 2014-07-24 15:30:24 --> Severity: Notice  --> Use of undefined constant MSGS_X_PAGE - assumed 'MSGS_X_PAGE' /var/www/dna2bpm/application/modules/inbox/controllers/inbox.php 61
