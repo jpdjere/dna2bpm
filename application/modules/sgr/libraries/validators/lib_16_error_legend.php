0ae6f0acf7e2ea2e755a8c9948e48b21dda93a65
Diego <daotero@industria.gob.ar>
2014-08-15 14:34:57 -0300

    fix isset $code

--- application/modules/sgr/libraries/validators/lib_16_error_legend.php
+++ application/modules/sgr/libraries/validators/lib_16_error_legend.php
@@ -3,14 +3,15 @@
 class Lib_16_error_legend {
 
     public function __construct() {
-        $this->result = $this->return_legend($code, $row, $value);
+        if (isset($code))
+            $this->result = $this->return_legend($code, $row, $value);
     }
 
     function return_legend($code, $row, $value) {
 
         switch ($code) {
             case "BJ.1":
-                $result_error = '<strong>Columna '.$code.' - Fila Nro.'. $row .' - Código Validación '.$code.'</strong><br/>Debe contener formato numérico sin decimales. Debe ser mayor o igual a cero.';
+                $result_error = '<strong>Columna ' . $code . ' - Fila Nro.' . $row . ' - Código Validación ' . $code . '</strong><br/>Debe contener formato numérico sin decimales. Debe ser mayor o igual a cero.';
                 break;
         }
         return $result_error;
