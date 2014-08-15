02f1dc25c5bb22cd81cea4ad583730abc2463c91
Juan Borda <juanignacioborda@gmail.com>
2014-08-15 11:43:42 -0300

    Extra aplicaciones dna2

--- application/modules/dashboard/assets/jscript/app.js
+++ application/modules/dashboard/assets/jscript/app.js
@@ -4,11 +4,59 @@
  **/
 
 $(document).ready(function() {
+    (function($) {
+        "use strict";
 
-$('.form-extra').ajaxForm({
-    target:'#tiles_after section',
-    replaceTarget:false
-});
+        $.fn.tree = function() {
+
+            return this.each(function() {
+                var btn = $(this).children("a").first();
+                var menu = $(this).children(".treeview-menu").first();
+                var isActive = $(this).hasClass('active');
+
+                //initialize already active menus
+                if (isActive) {
+                    menu.show();
+                    btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+                }
+                //Slide open or close the menu on link click
+                btn.click(function(e) {
+                    e.preventDefault();
+                    if (isActive) {
+                        //Slide up to close menu
+                        menu.slideUp();
+                        isActive = false;
+                        btn.children(".fa-angle-down").first().removeClass("fa-angle-down").addClass("fa-angle-left");
+                        btn.parent("li").removeClass("active");
+                    } else {
+                        //Slide down to open menu
+                        menu.slideDown();
+                        isActive = true;
+                        btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+                        btn.parent("li").addClass("active");
+                    }
+                });
+
+                /* Add margins to submenu elements to give it a tree look */
+                menu.find("li > a").each(function() {
+                    var pad = parseInt($(this).css("margin-left")) + 10;
+
+                    $(this).css({"margin-left": pad + "px"});
+                });
+
+            });
+
+        };
+
+
+    }(jQuery));
+    /* Sidebar tree view */
+    $(".sidebar .treeview").tree();
+
+    $('.form-extra').ajaxForm({
+        target: '#tiles_after section',
+        replaceTarget: false
+    });
     // ==== Reload Widget
     $(document).on('click', '.reload_widget', function(event) {
         event.preventDefault();
@@ -23,7 +71,7 @@ $('.form-extra').ajaxForm({
         });
     });
 
- // ==== Load Tiles 
+    // ==== Load Tiles 
     $(document).on('click', '.load_tiles_after', function(event) {
         event.preventDefault();
         var box = $(this).parents('.box');
@@ -34,34 +82,34 @@ $('.form-extra').ajaxForm({
             context: box
         })
                 .done(function(data) {
-            $('#tiles_after section').html(data);
-        })
-                .error(function(jqXHR,textStatus,errorThrown){
-                    $('#tiles_after section').html(textStatus+errorThrown);
+                    $('#tiles_after section').html(data);
                 })
-        ;
+                .error(function(jqXHR, textStatus, errorThrown) {
+                    $('#tiles_after section').html(textStatus + errorThrown);
+                })
+                ;
     });
-    
+
 // ==== Load Modal 
     $(document).on('click', '.load_modal', function(event) {
         event.preventDefault();
 
         var url = $(this).attr('href');
-        var title= $(this).attr('title')?($(this).attr('title')):('Title');
+        var title = $(this).attr('title') ? ($(this).attr('title')) : ('Title');
         $.ajax({
             url: url,
             context: document.body
         })
                 .done(function(data) {
-            $('#myModal').find('.modal-title').html(title);
-            $('#myModal').find('.modal-body').html(data);
-            $('#myModal').modal('show');
+                    $('#myModal').find('.modal-title').html(title);
+                    $('#myModal').find('.modal-body').html(data);
+                    $('#myModal').modal('show');
 
-        })
-                .error(function(jqXHR,textStatus,errorThrown){
-                    $('#tiles_after section').html(textStatus+errorThrown);
                 })
-        ;
+                .error(function(jqXHR, textStatus, errorThrown) {
+                    $('#tiles_after section').html(textStatus + errorThrown);
+                })
+                ;
     });
 
 // ==== Make the dashboard widgets sortable Using jquery UI
@@ -82,24 +130,24 @@ $('.form-extra').ajaxForm({
     }).disableSelection();
     ;
 
-  //=========== ICHECK 
+    //=========== ICHECK 
 
-  //iCheck for checkbox and radio inputs
-  $('input[type="checkbox"]').iCheck({
-      checkboxClass: 'icheckbox_minimal',
-      radioClass: 'iradio_minimal'
-  });
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"]').iCheck({
+        checkboxClass: 'icheckbox_minimal',
+        radioClass: 'iradio_minimal'
+    });
 
-  //When unchecking the checkbox
-  $(document).on('ifUnchecked', "#check-all", function(event) {
-      //Uncheck all checkboxes
-      $("input[type='checkbox']", ".table-mailbox").iCheck("uncheck");
-  });
+    //When unchecking the checkbox
+    $(document).on('ifUnchecked', "#check-all", function(event) {
+        //Uncheck all checkboxes
+        $("input[type='checkbox']", ".table-mailbox").iCheck("uncheck");
+    });
 
-  //When checking the checkbox
-  $("#check-all").on('ifChecked', function(event) {
-  	 $("input[type='checkbox']", ".table-mailbox").iCheck("check");
-  });
+    //When checking the checkbox
+    $("#check-all").on('ifChecked', function(event) {
+        $("input[type='checkbox']", ".table-mailbox").iCheck("check");
+    });
 
 
 
--- application/modules/dashboard/libraries/menu_extra.php
+++ application/modules/dashboard/libraries/menu_extra.php
@@ -18,11 +18,44 @@ class Menu_extra {
     public function __construct($params = array()) {
 // Set the super object to a local variable for use throughout the class
         $this->CI = & get_instance();
+                //---base variables
+        $this->base_url = base_url();
+        $this->idu = (int) $this->CI->session->userdata('iduser');
         // Register Scripts
     }
 
     public function get() {
-    return 'H1-H1-H1-H1-H1-H1-H1-';    
+        $this_CI=& get_instance();
+        $this_CI->load->model('app');
+        $cpData = array();
+        $user = $this_CI->user->get_user($this->idu);
+        $apps = $this_CI->app->get_apps();
+
+        if ($apps->count()) {
+            //----check if the user has access to thi app
+            foreach ($apps as $thisApp) {
+                $authorized = false;
+                if (isset($thisApp['groups'])) {
+                    foreach ($thisApp['groups'] as $idgroup) {
+                        if (in_array($idgroup, $user->group)) {
+                            $authorized = true;
+                            break;
+                        }
+                    }
+                }
+                //if ($this->user->has('root/modules/application/' . $thisApp['idapp']) or $this->user->isAdmin($user)) {
+                if ($authorized or $this_CI->user->isAdmin($user)) {
+                    $cpData['apps'][] = array(
+                        'icon' => isset($thisApp['icon']) ? $thisApp['icon'] : 'icon-list-alt',
+                        'name' => isset($thisApp['title']) ? $thisApp['title'] : $thisApp['idapp'] . '(???)',
+                        'link' => $this->base_url . 'dna2/application/' . $thisApp['idapp'],
+                        'target' => '_self'
+                    );
+                }
+            }
+            $retStr=$this_CI->parser->parse('dashboard/menu_extra_dna2',$cpData,true,true);
+            return $retStr;
+        }
     }
 
 }
