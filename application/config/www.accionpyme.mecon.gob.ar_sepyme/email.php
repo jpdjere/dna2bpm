e8d7a796e405cdc7762d12395faf64b320d56827
Juan Borda <juanignacioborda@gmail.com>
2014-08-22 17:05:32 -0300

    SMTP Configs

---
+++ application/config/www.accionpyme.mecon.gob.ar_sepyme/email.php
@@ -0,0 +1,11 @@
+<?php
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = '10.25.0.15';
+$config['smtp_user'] = 'dna2@industria.gob.ar';
+$config['smtp_user_name'] = 'DNA²';
+$config['smtp_pass'] = '';
+$config['smtp_timeout'] = '30';
+$config['protocol'] = 'smtp';
+$config['charset'] = 'UTF-8';
+$config['wordwrap'] = TRUE;
+$config['mail_suffix'] = 'DNA2BPM';
