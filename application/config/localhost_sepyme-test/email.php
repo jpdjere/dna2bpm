9bf10d38124c481dec911e1c9940601f822fa927
Juan Ignacio Borda <juanignacioborda@gmail.com>
2014-10-01 17:26:27 -0300

    email config para local

---
+++ application/config/localhost_sepyme-test/email.php
@@ -0,0 +1,14 @@
+<?php
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'localhost';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['smtp_timeout'] = '30';
+$config['protocol'] = 'smtp';
+$config['charset'] = 'UTF-8';
+$config['wordwrap'] = TRUE;
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
