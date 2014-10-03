c39341115a90fc0c29539eaf64e289365944d597
Diego <daotero@industria.gob.ar>
2014-10-03 11:54:14 -0300

    fix templates anexos 12x

--- application/modules/sgr/models/model_121.php
+++ application/modules/sgr/models/model_121.php
@@ -171,33 +171,14 @@ class Model_121 extends CI_Model {
     }
 
     function get_anexo_info($anexo, $parameter) {
-        $tmpl = array(
-            'data' => '<tr><td colspan="2" align="center">Garantía.</td>
-                                <td colspan="2" align="center">Del Participe / Beneficiario</td>
-                                <td colspan="3" align="center">Información sobre la Amortización</td>                                
-                            </tr>                               
-                            <tr>
-                                <td>N° de Orden de<br/>la Garantía<br/>Otorgada</td>
-                                <td>N° de<br/>Cuota</td>
-                                <td>C.U.I.T.</td>
-                                <td>Apellido y Nombre o Razón<br/>Social</td>
-                                <td>Fecha de<br/>Vencimiento<br/>de la Cuota</td>
-                                <td>Monto de la<br/>Cuota de la<br/>Garantía</td>
-                                <td>Monto de la<br/>Cuota del<br/>Importe Menor</td>
-                            </tr>
-                            <tr>
-                                <th>1</th>
-                                <th>2</th>
-                                <th>3</th>
-                                <th>4</th>
-                                <th>5</th>
-                                <th>6</th>
-                                <th>7</th>                                               
-                            </tr>',
-        );
+        /* HEADER TEMPLATE */
+        $header_data = array();
 
+        $header = $this->parser->parse('prints/anexo_' . $anexo . '_header', TRUE);
+        $tmpl = array('data' => $header);
 
         $data = array($tmpl);
+
         $anexoValues = $this->get_anexo_data($anexo, $parameter);
         foreach ($anexoValues as $values) {
             $data[] = array_values($values);
---
+++ application/modules/sgr/views/prints/anexo_124_header.php
@@ -0,0 +1,43 @@
+<tr><td colspan="13">GARANTIAS REAFIANZADAS</td></tr><tr><td rowspan="5">N&ordm; de Orden</td>
+    <td colspan="2" rowspan="3">Del Part&iacute;cipe / Beneficiario</td>
+    <td colspan="3" rowspan="3">De la Garant&iacute;a</td>
+
+    <td colspan="2" rowspan="3">Del Acreedor</td>
+    <td colspan="3" rowspan="3">Del Reafianzamiento</td>
+    <td colspan="2" rowspan="3">Institución Reafianzadora</td>
+</tr>
+<tr></tr>
+<tr></tr>
+<tr>
+    <td rowspan="2">Nombre o raz&oacute;n social</td>
+    <td rowspan="2">C.U.I.T.</td>
+    <td rowspan="2">Fecha de    origen</td>
+    <td rowspan="2">Tipo</td>                                
+    <td rowspan="2">Importe</td>
+    <td rowspan="2">Nombre o Razón Social</td>                                
+    <td rowspan="2">C.U.I.T.</td>
+    <td rowspan="2">Fecha de<br>entrada en Vigencia</td>
+    <td rowspan="2">Saldo Vigente</td>
+    <td rowspan="2">Porcentaje Reafianzado</td>
+    <td rowspan="2">Razón Social</td>
+    <td rowspan="2">C.U.I.T.</td>                                                             
+</tr>
+<tr>
+
+
+</tr>
+<tr>
+    <th>1</th>
+    <th>2</th>
+    <th>3</th>
+    <th>4</th>
+    <th>5</th>
+    <th>6</th>
+    <th>7</th>
+    <th>8</th>
+    <th>9</th>
+    <th>10</th>
+    <th>11</th>
+    <th>12</th>
+    <th>13</th>                                           
+</tr>
