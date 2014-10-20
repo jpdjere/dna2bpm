acf901cb70cb440be5a366910535b065e64e902e
Juan Ignacio Borda <juanignacioborda@gmail.com>
2014-10-20 17:02:24 -0300

    Hago Dump de Notificación con idwf,idcase, resourceId

--- application/modules/fondyf/controllers/test.php
+++ application/modules/fondyf/controllers/test.php
@@ -18,6 +18,7 @@ class test extends MX_Controller {
         $this->load->library('cimongo/cimongo');
         $this->db = $this->cimongo;
         $this->dna2 = $this->load->database('dna2', true, true);
+        $this->lang->load('library', $this->config->item('language'));
         $this->user->authorize();
     }
 
@@ -80,6 +81,56 @@ class test extends MX_Controller {
         }
     }
 
+    function notificacion($idwf, $idcase, $resourceId) {
+        $this->load->model('bpm/bpm');
+        $this->load->library('parser');
+//        $this->bpm->debug['load_case_data'] = true;
+        $user=$this->user->getuser((int) $this->session->userdata('iduser'));
+        $case = $this->bpm->get_case($idcase, $idwf);
+        $this->user->Initiator=$case['iduser'];
+        $token = $this->bpm->get_token($idwf, $idcase, $resourceId);
+        //---saco título para el resultado
+        $mywf = $this->bpm->load($idwf);
+        $wf = $this->bpm->bindArrayToObject($mywf ['data']);
+        //---tomo el template de la tarea
+        $shape = $this->bpm->get_shape($resourceId, $wf);
+
+        $data = $this->bpm->load_case_data($case, $idwf);
+        $data['user'] = (array) $user;
+        $data['date'] = date($this->lang->line('dateFmt'));
+        $msg['from'] = $this->idu;
+        $msg['subject'] = $this->parser->parse_string($shape->properties->name, $data, true, true);
+        $msg['body'] = $this->parser->parse_string($shape->properties->documentation, $data, true, true);
+
+        $msg['idwf'] = $idwf;
+        $msg['case'] = $idcase;
+        if ($shape->properties->properties <> '') {
+            foreach ($shape->properties->properties->items as $property) {
+                $msg[$property->name] = $property->datastate;
+            }
+        }
+        $resources = $this->bpm->get_resources($shape, $wf, $case);
+        //---if has no messageref and noone is assigned then
+        //---fire a message to lane or self         
+//            if (!count($resources['assign']) and !$shape->properties->messageref) {
+//                $lane = $this->bpm->find_parent($shape, 'Lane', $wf);
+//                //---try to get resources from lane
+//                if ($lane) {
+//                    $resources = $this->bpm->get_resources($lane, $wf);
+//                }
+//                //---if can't get resources from lane then assign it self as destinatary
+//                if (!count($resources['assign']))
+//                    $resources['assign'][] = $this->user->Initiator;
+//            }
+        //---process inbox--------------
+        $to = (isset($resources['assign'])) ? array_merge($token['assign'], $resources['assign']) : $token['assign'];
+        $to = array_unique(array_filter($to));
+        foreach($to as $iduser){
+            $user=$this->user->get_user_safe($iduser);
+            $msg['to'][]=$user;
+        }
+        var_dump($msg);
+    }
 
 }
 
