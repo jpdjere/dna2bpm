ddaca6722dbf6a99807ff5b68ae30c9f9316a6a3
Diego Vaio <xxcynicxx@gmail.com>
2014-11-03 18:19:27 -0300

    ws get value

--- application/modules/webservice/libraries/programs/crefis.php
+++ application/modules/webservice/libraries/programs/crefis.php
@@ -19,28 +19,10 @@ class CreFis {
 	public $self = false; // si es true son empresas si no otra entidad
 
 	function monto($idrel) {
-		return $this->getvalue ( $idrel, 5040 );
+		return getvalue ( $idrel, 5040 );
 	}
 
 }
 
-function getvalue($id, $idframe) {
 
-	$rtnVal = null;
-	$result = null;
-	$id = (double) $id;
-	$idframe = (int) $idframe;
-	//----Get container
-	$frame = $this->mongo->db->frames->findOne(array('idframe' => $idframe), array('container'));
-	$query = array('id' => $id);
-	$fields = array((string) $idframe);
-	if ($frame['container']) {
-		$result = $this->mongo->db->selectCollection($frame['container'])->findOne($query, $fields);
-	} else {
-		trigger_error("container property missing for: $idframe");
-	}
-	//var_dump($frame['container'],json_encode($query),json_encode($fields),$result);
-	$rtnVal = (isset($result[$idframe])) ? $result[$idframe] : null;
-	return $rtnVal;
-}
 
