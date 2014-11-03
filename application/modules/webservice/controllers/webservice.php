89aceaf82edfdf05f5f211294310fcd3649ad1f7
Diego Vaio <xxcynicxx@gmail.com>
2014-11-03 15:49:20 -0300

    ws programs

--- application/modules/webservice/controllers/webservice.php
+++ application/modules/webservice/controllers/webservice.php
@@ -12,23 +12,7 @@ if (!defined('BASEPATH'))
  * @date   Oct 21, 2014
  */
 
-class CreFis_UCAP {
 
-	public $nombre = 'Credito Fiscal - UCAP';
-	public $cod = 'CreFis';
-	public $where = array (
-			4846
-	);
-	public $id = 4837;
-	public $titulo = 4842;
-	public $url = 'frontcustom/231/interno.print.php?a=1';
-	public $estado = 4970;
-
-	function monto($idrel) {
-		return getvalue ( $idrel, 5040 );
-	}
-
-}
 
 class Webservice extends MX_Controller {
 
