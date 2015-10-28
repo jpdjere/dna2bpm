3ec1d7d2bb645bff5349057dd8eba8c7398d96d9
gabriel <gabriel@trialvd.com.ar>
2015-10-28 17:37:10 -0300

    change of BT version

--- application/modules/siac/views/bootstrap.ui.php
+++ application/modules/siac/views/bootstrap.ui.php
@@ -6,12 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link href="{base_url}jscript/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Latest compiled and minified CSS -->
+        <link href="{base_url}jscript/bootstrap-wysihtml5/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="{base_url}jscript/fontawesome/css/font-awesome.min.css" />
         <!-- CSS -->
         {css}
 
-        <link href="{base_url}jscript/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <!--<link href="{base_url}jscript/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">-->
     </head>
     <body>
         {content}
@@ -21,7 +22,7 @@
             var globals={inline_js};
         </script>
         <script type="text/javascript" src="{base_url}jscript/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="{base_url}jscript/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="{base_url}jscript/bootstrap-wysihtml5/js/bootstrap.min.js"></script>
         {js}
     </body>
 </html>
--- application/modules/siac/views/formulario.php
+++ application/modules/siac/views/formulario.php
@@ -8,11 +8,11 @@
 
         <!-- Multiple Radios -->
         <div class="form-group">
-            <label class="col-md-4 control-label" for="radios">Multiple Radios</label>
+            <label class="col-md-4 label-control" for="radios">Multiple Radios</label>
             <div class="col-md-4">
                 <div class="radio">
                     <label for="radios-0">
-                        <input type="radio" name="radios" id="radios-0" value="1" checked="checked"> Alumbrado
+                        <input  type="radio" name="radios" id="radios-0" value="1" checked="checked"> Alumbrado
                     </label>
                 </div>
                 <div class="radio">
@@ -22,7 +22,7 @@
                 </div>
                 <div class="radio">
                     <label for="radios-2">
-                        <input type="radio" name="radios" id="radios-2" value="3"> Vía pública (calles, veredas,ramblas, etc)
+                        <input  type="radio" name="radios" id="radios-2" value="3"> Vía pública (calles, veredas,ramblas, etc)
                     </label>
                 </div>
                 <div class="radio">
@@ -32,12 +32,12 @@
                 </div>
                 <div class="radio">
                     <label for="radios-4">
-                        <input type="radio" name="radios" id="radios-4" value="5"> Limpieza y Recolección
+                        <input  type="radio" name="radios" id="radios-4" value="5"> Limpieza y Recolección
                     </label>
                 </div>
                 <div class="radio">
                     <label for="radios-5">
-                        <input type="radio" name="radios" id="radios-5" value="6"> Residuos Especiales
+                        <input  type="radio" name="radios" id="radios-5" value="6"> Residuos Especiales
                     </label>
                 </div>
             </div>
@@ -45,7 +45,7 @@
 
         <!-- Textarea -->
         <div class="form-group">
-            <label class="col-md-4 control-label" for="textarea">Descripción</label>
+            <label class="col-md-4 " for="textarea">Descripción</label>
             <div class="col-md-4">
                 <textarea class="form-control" id="textarea" name="textarea"></textarea>
             </div>
@@ -53,7 +53,7 @@
 
         <!-- Text input-->
         <div class="form-group">
-            <label class="col-md-4 control-label" for="email">Direccion de orreo</label>
+            <label class="col-md-4 " for="email">Direccion de orreo</label>
             <div class="col-md-4">
                 <input id="email" name="email" type="text" placeholder="juanperez@argentina.com.ar" class="form-control input-md" required="">
                 <span class="help-block">Ingrese aquí su dirección de correo electrónico</span>
@@ -62,7 +62,7 @@
 
         <!-- Text input-->
         <div class="form-group">
-            <label class="col-md-4 control-label" for="telefono">Teléfono</label>
+            <label class="col-md-4 " for="telefono">Teléfono</label>
             <div class="col-md-4">
                 <input id="telefono" name="telefono" type="text" placeholder="0244-7356-2236" class="form-control input-md" required="">
                 <span class="help-block">ingrese un teléfono de contacto</span>
@@ -71,7 +71,7 @@
 
         <!-- Button -->
         <div class="form-group">
-            <label class="col-md-4 control-label" for="enviar"></label>
+            <label class="col-md-4 " for="enviar"></label>
             <div class="col-md-4">
                 <button type="submit" id="enviar" name="enviar" class="btn btn-success">Enviar</button>
             </div>
