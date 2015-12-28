6c0c9b22deae79d69473cdb35b0cfd3b542f8abe
Juan Ignacio Borda <juanignacioborda@gmail.com>
2015-12-28 17:30:47 -0300

    add composer support

---
+++ application/libraries/composer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of MY_Composer
+ *
+ * @author Rana
+ */
+class Composer 
+{
+    function __construct() 
+    {
+        include(APPPATH."vendor/autoload.php");
+    }
+}
