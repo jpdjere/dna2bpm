ab71535a8965938cb024982329f086baee66f651
Juan Borda <juanignacioborda@gmail.com>
2016-03-31 12:22:13 -0300

    fix presentacion

--- application/modules/presentacion/controllers/Presentacion.php
+++ application/modules/presentacion/controllers/Presentacion.php
@@ -7,11 +7,13 @@ class Presentacion extends MX_Controller {
 
     function __construct() {
         parent::__construct();
-        //$this->user->authorize();
+    }
+
+    function index() {
         $this->load->library('parser');
         $parts=$this->uri->segments;
         unset($parts[1]);
-        
+        // var_dump($parts);
         //---base variables
         $data['base_url'] = base_url();
         $data['module_url'] = base_url() . $this->router->fetch_module() . '/';
@@ -22,19 +24,14 @@ class Presentacion extends MX_Controller {
         //----get path parts form extension
         // $path_parts = pathinfo($file);
         //var_dump($parts,$data);
-        
+
         echo $this->parser->parse('presentacion/template',$data);
         exit;
     }
-
-    function index() {
-        $this->fdefault();
-    }
-
     function fdefault(){
         $this->load->view('default');
     }
-
+    function dna2(){
+        $this->index();
+    }
 }
-
-?>
