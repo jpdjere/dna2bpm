bd64173554780020d2d68a5877034f423a571172
Diego <daotero@produccion.gob.ar>
2016-03-31 13:45:16 -0300

    check gtw

--- application/modules/bpm/controllers/Gateway.php
+++ application/modules/bpm/controllers/Gateway.php
@@ -19,10 +19,10 @@ class Gateway extends CI_Controller {
         parent::__construct();
         session_start();
         
-        var_dump($_REQUEST['url']);
+       
         
         if (isset($_REQUEST['url'])) {
-            $url =  base64_decode(urldecode($_REQUEST['url']));
+            $url =   var_dump($_REQUEST['url']);//base64_decode(urldecode($_REQUEST['url']));
             $_SESSION['idu'] = $this->session->userdata('iduser');
             //echo $url;exit;
             redirect($url);
