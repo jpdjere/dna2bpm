cf80facd50315dc31c36cf492d9b83cb56da7463
Diego <daotero@produccion.gob.ar>
2016-04-06 15:45:49 -0300

    Xls_asset

--- application/modules/sgr/controllers/Xls_asset.php
+++ application/modules/sgr/controllers/Xls_asset.php
@@ -7,33 +7,59 @@ if (!defined('BASEPATH'))
  * ASSETS Controller
  * This file allows you to  access assets from within your modules directory
  * 
- * @author Diego
+ * @author Diego Otero
  * 
- * @version 	1.0 (2012-05-27)
+ * @version 	2.0 (2016-04-06)
  * 
  */
 
-class Xls_asset extends CI_Controller {
+class Xls_asset extends MX_Controller {
 
     function __construct() {
         parent::__construct();
+       
+    }
+    
+    function index(){
         //$this->user->authorize();
+         
+         if(count($this->uri->segments)==2){
+             show_error("Serving assets for: ".APPPATH. 'modules/' . implode('/', $this->uri->segments));
+             exit;
+         }
+         
         //---get working directory and map it to your module
+        
         $var = array_shift($this->uri->segments);
         $var = array_shift($this->uri->segments);
-        $file = getcwd() . '/anexos_sgr/' . implode('/', $this->uri->segments);
+        $var = array_shift($this->uri->segments);
+        $var = array_shift($this->uri->segments);
+        
+        /*Anexo NUM*/
+        $anexo_num = $var;
+        
+        $filename = implode('/', $this->uri->segments);
+        
+         
+        
+        $file = getcwd() . '/anexos_sgr/'.$anexo_num."/" . $filename;
         $file = str_replace("%20", " ", $file);
         //----get path parts form extension
-        $path_parts = pathinfo( $file);
+        $path_parts = pathinfo($file);
+        
+     
         //---set the type for the headers
         $file_type=  strtolower($path_parts['extension']);        
         if (is_file($file)) {
             header('Content-type: application/vnd.ms-excel'); 
             readfile($file);
-        } else {
-            echo "file" .$file;
+              
+        } else { 
+             show_error("Error getting files: ".str_replace("%20", " ", $filename));
+             exit;
            
         }
         exit;
     }
+
 }
