4f0637b7d39968a94a118ca3c805a60fe9c5017d
gabriel <gabriel@trialvd.com.ar>
2016-04-07 16:13:16 -0300

    ups

---
+++ application/modules/dashboard/views/_logos_header.php
@@ -0,0 +1,6 @@
+<!---- Logos -->
+    <div class="header_institucional">
+      <img src="{base_url}dashboard/assets/img/logo_presidencia.png" class="presidencia_logo">
+      <img src="{base_url}dashboard/assets/img/logo_secretaria.png" class="secretaria_logo">
+    </div>
+<!---- /Logos -->
