4b2ca821141fa64790d41487afd3caee1017c5ba
mmedina <mmedina@sepyme.gov.ar>
2016-04-13 15:47:22 -0300

    cambio en el tile buscar

--- application/modules/crefis/views/buscar_proyecto.php
+++ application/modules/crefis/views/buscar_proyecto.php
@@ -6,7 +6,7 @@
         <h4>Buscador</h4>
         <form class="form-extra" accept-charset="utf-8" method="post" action="{base_url}crefis/buscar/pp">
             <div class="col-lg-9 input-group input-group-sm">
-                <span class="input-group-addon">#PP</span>
+                <span class="input-group-addon">Nro.</span>
                 <input type="text"  class="form-control" name="query" placeholder="ej: 003/1014 ó nombre empresa ó cuit:30-11634893-7" />
                 <span class="input-group-btn">
                     <button type="submit" class="btn btn-info btn-flat btn-search">Buscar</button>
