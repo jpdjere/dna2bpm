4341c2429452549c579fce33b220b8e92591ed82
gabriel <gabriel@trialvd.com.ar>
2016-04-19 13:22:14 -0300

    test recover

--- application/modules/user/controllers/Recover.php
+++ application/modules/user/controllers/Recover.php
@@ -65,7 +65,7 @@ class Recover extends MX_Controller {
     
     function Send() {
         //----LOAD LANGUAGE
-        
+        ini_set('display_errors',1);
         $this->load->model('msg');
         $this->lang->load('login', $this->config->item('language'));
         
