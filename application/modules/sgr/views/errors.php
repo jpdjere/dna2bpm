68a3f91d9d16edc344980e30c546ac3322276b62
Diego <daotero@produccion.gob.ar>
2016-05-04 15:44:04 -0300

    logos sgr

--- application/modules/sgr/views/errors.php
+++ application/modules/sgr/views/errors.php
@@ -13,9 +13,9 @@
         </ul>
     </div>
 
-    <div id="header">
-        <div id="header-dna"></div>
-        <div id="header-logos"></div>
+    <div class="header_institucional">
+      <img src="{base_url}dashboard/assets/img/logo_presidencia.png" class="presidencia_logo">
+      <img src="{base_url}dashboard/assets/img/logo_secretaria.png" class="secretaria_logo">
     </div>
 
      <h2><i class="fa fa-bars"></i> Errores en la importación de {anexo_title_cap} </h2>
