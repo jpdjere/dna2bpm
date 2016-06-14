44342990cb873fb577be284680e0c4892b843a3c
martin@mglongchamps <mglongchamps@gmail.com>
2016-06-14 10:00:44 -0300

    Dash Expertos

--- application/modules/expertos/controllers/Expertos.php
+++ application/modules/expertos/controllers/Expertos.php
@@ -1399,8 +1399,8 @@ BLOCK;
         $query = array(
             //'assign' => $this->idu,
             'status' => 'user',
-            //'idwf' =>array('$in'=>array('Expertos_Base','carga_pro_inst')) 
-            'idwf' => 'Expertos_Base'
+            'idwf' =>array('$in'=>array('Expertos_Base','carga_pro_inst')) 
+            //'idwf' => 'Expertos_Base'
         );
 
         //var_dump(json_encode($query));exit;
