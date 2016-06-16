0bd1807540382a15fd893232e56886194aa17c41
martin@mglongchamps <mglongchamps@gmail.com>
2016-06-16 17:10:28 -0300

    Mass

--- application/modules/expertos/controllers/Expertos.php
+++ application/modules/expertos/controllers/Expertos.php
@@ -1500,7 +1500,7 @@ BLOCK;
         foreach($cases as $case){
             var_dump($case);
             $this->user->idu=$case['iduser'];
-            //$this->case_manager->revert('model', 'Expertos_Base', $case['id'], $resourceId);
+            $this->case_manager->revert('model', 'Expertos_Base', $case['id'], $resourceId);
             //$this->engine->Run('model', 'Expertos_Base', $case['id'],null, true);
             if($case['token_status']['oryx_D86216E3-A7DA-49DF-9886-AE1028BF67DD']== "pending"){
                 $this->engine->Run('model', 'Expertos_Base', $case['id'],null, true);
