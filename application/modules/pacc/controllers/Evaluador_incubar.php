4eab6a1854a4df9a9bb94950ac13767bde47e37f
giritcity_mi <giritcity_mi@PC1107244>
2016-06-21 13:01:56 -0300

    Busco error

--- application/modules/pacc/controllers/Evaluador_incubar.php
+++ application/modules/pacc/controllers/Evaluador_incubar.php
@@ -44,8 +44,7 @@ class Evaluador_incubar extends MX_Controller {
         $this->user->authorize();
         $this->load->library('parser');
         $template = 'pacc/listar_agencias';
-echo'XXXXXXXXXXXXXXXXX';
-exit;
+
         $filter = array(
             'idwf' => 'INCUBAR',
             'resourceId' =>$this->consolida_resrourceId
