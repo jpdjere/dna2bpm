f62f6a27fe5c472171236424576a522878638204
giritcity_mi <giritcity_mi@PC1107244>
2016-06-21 11:33:16 -0300

    Busco error en buscador

--- application/modules/pacc/controllers/Evaluador_incubar.php
+++ application/modules/pacc/controllers/Evaluador_incubar.php
@@ -8,7 +8,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class evaluador_incubar extends MX_Controller {
+class Evaluador_incubar extends MX_Controller {
 
     function __construct() {
         parent::__construct();
