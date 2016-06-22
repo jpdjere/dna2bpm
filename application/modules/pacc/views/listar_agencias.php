7c775e1f96a6e7c4e46fb46f358b294c0baa323f
giritcity_mi <giritcity_mi@PC1107244>
2016-06-22 10:06:35 -0300

    Actualizo url para ver Preinscripción

--- application/modules/pacc/views/listar_agencias.php
+++ application/modules/pacc/views/listar_agencias.php
@@ -33,7 +33,7 @@
                         </a>{/if}
                     </td>
                     <td>
-                        <a href="{url_ver}" target="_blank" title="Ver Pre-Insc. del Proyecto">
+                        <a href="{link_msg}" target="_blank" title="Ver Pre-Insc. del Proyecto">
                             <i class="ion ion-folder fa-2x fa-adjust"></i>
                         </a>
                     </td>
