a613d525989227ac3a7044904614d761033f5793
sblazquez <seby_1996@hotmail.com>
2016-07-13 13:29:06 -0300

    Bonita: seactualizan los reportes

--- application/modules/bonita/models/Model_bonita_reportes.php
+++ application/modules/bonita/models/Model_bonita_reportes.php
@@ -15,20 +15,7 @@ class model_bonita_reportes extends CI_Model {
         parent::__construct();
         //$this->load->config('importar');
         //var_dump($this->config);exit;
-        $config['hostname'] = 'localhost';
-        $config['username'] = 'root';
-        $config['password'] = 'seba1553';
-        $config['database'] = 'importar';
-        $config['dbdriver'] = 'mysqli';
-        $config['dbprefix'] = '';
-        $config['pconnect'] = FALSE;
-        $config['db_debug'] = TRUE;
-        $config['cache_on'] = FALSE;
-        $config['cachedir'] = '';
-        $config['char_set'] = 'utf8';
-        $config['dbcollat'] = 'utf8_general_ci';
-        
-        $this->db_importar = $this->load->database($config,TRUE);
+        $this->db_importar = $this->load->database('importar',TRUE);
     }
     
     /**
