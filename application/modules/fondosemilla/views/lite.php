baa9514ca8ca1c2bb059972b5bb757c8d572684a
Luciano Menez <lucianomenez1212@gmail.com>
2016-07-20 17:54:28 -0300

    saco el texto explicativo

--- application/modules/fondosemilla/views/lite.php
+++ application/modules/fondosemilla/views/lite.php
@@ -64,7 +64,7 @@
         <h4 class="modal-title">Cargá tu proyecto en Fondo Semilla</h4>
       </div>
       <div class="modal-body">
-        <p>Texto Explicativo---- Lorem ipsum</p>
+        <p></p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
