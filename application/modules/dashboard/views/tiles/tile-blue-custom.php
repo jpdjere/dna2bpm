67ce5d583d9149bb8a8b4ac1583ea6fa32cbc00b
Luciano Menez <lucianomenez1212@gmail.com>
2016-07-27 16:22:37 -0300

    agrego un tile

---
+++ application/modules/dashboard/views/tiles/tile-blue-custom.php
@@ -0,0 +1,20 @@
+<div class="small-box bg-blue {update_class}" id="{id}">
+<span class="hidden widget_url">{widget_url}</span>
+    <div class="inner">
+        <h3>
+            {number}
+        </h3>
+        <p>
+            {title}
+        </p>
+    </div>
+    <div class="icon">
+        <i class="ion {icon}"></i>
+    </div>
+    <a class="small-box-footer {more_info_class}" href="{more_info_link}">
+        {more_info_text} <i class="fa fa-arrow-circle-right"></i>
+    </a>
+    <a class="small-box-footer {more_info_class}" href="{base_url}fondosemilla/semilla/exportar_xls/{idkpi}">
+        Exportar a XLS <i class="fa fa-arrow-circle-right"></i>
+    </a>    
+</div>
