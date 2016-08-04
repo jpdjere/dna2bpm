d74a0b393e78769b411f9fa2559f4eee4fa2d004
martin@mglongchamps <mglongchamps@gmail.com>
2016-08-04 16:12:43 -0300

    Modificamos cambios Juan

--- application/modules/perfil/controllers/Api.php
+++ application/modules/perfil/controllers/Api.php
@@ -57,7 +57,7 @@ class Api extends MX_Controller {
             foreach ($result as $key => $value) { 
                 foreach ($value as $cuit=>$date) {  
                     //$procesos=$this->eventanilla_model->get_process(array('cuit'=>$cuit),array('denominacion'),array('denominacion'=>'ASC'));                                 
-                    $procesos=$this->eventanilla_model->get_process(array('cuit'=>$cuit));                                 
+                    $procesos=$this->eventanilla_model->get_process(array('cuit'=>$cuit),array('denominacion'));                                 
                     $data[] =array(
                         'cuit'=>$cuit,
                         'razon_social'=>$procesos[0]->denominacion,
