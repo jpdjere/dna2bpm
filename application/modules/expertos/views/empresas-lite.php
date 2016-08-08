09c7e135fb8c9fbd9cb11d8f13523dc4be26a1c2
Luciano Menez <lucianomenez1212@gmail.com>
2016-08-08 13:22:01 -0300

    fix leyenda

--- application/modules/expertos/views/empresas-lite.php
+++ application/modules/expertos/views/empresas-lite.php
@@ -61,7 +61,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Solicitud de Asistencia!</h4>
+        <h4 class="modal-title">Solicitud de Asistencia Técnica</h4>
       </div>
       <div class="modal-body">
         <p></p>
