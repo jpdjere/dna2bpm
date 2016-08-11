691079e8f00e58be5c1f02dbc390adf222ecfede
Juan Ignacio Borda <juanignacioborda@gmail.com>
2016-08-11 12:36:14 -0300

    fix afip

--- application/modules/afip/models/Consultas_model.php
+++ application/modules/afip/models/Consultas_model.php
@@ -50,7 +50,6 @@ class Consultas_model extends CI_Model {
      
        if(empty($cuit))return false;
        if($this->has_1273($cuit)==false)return false;
-       if(empty($this->isPyme($cuit)))return false;
        if($this->isPyme($cuit))return false;
 
        $query=array('cuit'=>new MongoInt64($cuit));
