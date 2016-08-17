8b6b9533b10351a5a6185d63772ebfb11647e5c6
gabriel <gabriel@trialvd.com.ar>
2016-08-17 15:16:57 -0300

    fix height

--- application/modules/perfil/views/hub.php
+++ application/modules/perfil/views/hub.php
@@ -5,7 +5,7 @@
         <div class="box-header">
             <h3 class="box-title">Registro Pyme</h3>
         </div>
-        <div class="box-body" style="min-height:170px">
+        <div class="box-body" style="min-height:200px">
 
            <p>Aplicá a todos los programas y beneficios de Financiamiento, Capacitación, Asistencia Técnica y Beneficios Impositivos que existen para las PyMEs desde el Portal.</p>
 <p>Dentro del Portal podrás aplicar a los programas:
@@ -29,7 +29,7 @@
         <div class="box-header">
             <h3 class="box-title">Registro Emprendedores</h3>
         </div>
-        <div class="box-body" style="min-height:170px">
+        <div class="box-body" style="min-height:200px">
 <p>Aplicá a los programas de Financiamiento, Capacitación y Asistencia que existen en el Portal de la Secretaría.
 </p>
 <p>Dentro del Portal podrás aplicar al programa:
@@ -52,7 +52,7 @@
         <div class="box-header">
             <h3 class="box-title">Registro Incubadoras</h3>
         </div>
-        <div class="box-body" style="min-height:170px">
+        <div class="box-body" style="min-height:200px">
             <p>Registrate como Incubadora y empezá a participar de los programas, actividades y eventos asociados al ecosistema emprendedor.</p><p>Para recibir proyectos de Fondo Semilla y PAC Emprendedor debés iniciar la inscripción aquí</p>
           
         </div>
@@ -68,7 +68,7 @@
         <div class="box-header">
             <h3 class="box-title">Registro Expertos</h3>
         </div>
-        <div class="box-body" style="min-height:170px">
+        <div class="box-body" style="min-height:200px">
             <p>Registrate como experto y participá del programa Expertos PyME para brindar consultorías y asistencia a las PyMEs</p>
         </div>
         <div class="box-footer">
