2cd53531256e6b7ef25da39b4bf68fe12841deca
gabriel <gabriel@trialvd.com.ar>
2016-08-17 15:12:27 -0300

    fix texts

--- application/modules/perfil/views/hub.php
+++ application/modules/perfil/views/hub.php
@@ -16,6 +16,25 @@
         </div>
     </div>
   </div>
+
+<!-- -->
+  <div class="col-md-3 " >
+    <div class="box box-solid box-primary" >
+        <div class="box-header">
+            <h3 class="box-title">Registro Emprendedores</h3>
+        </div>
+        <div class="box-body" style="min-height:170px">
+            <p>Aplicá a los programas de Financiamiento, Capacitación y Asistencia que existen en el Portal de la Secretaría.
+</p><p>Dentro del Portal podrás aplicar: al programa Fondo Semilla</p>
+
+        </div>
+            <div class="box-footer">
+            <a href="{base_url}perfil/registro_emprendedor" class="btn btn-success btn-block">Registrarme</a>
+        </div>
+    </div>
+  </div>
+
+
 <!-- -->
   <div class="col-md-3 " >
     <div class="box box-solid box-primary" >
@@ -31,27 +50,12 @@
         </div>
     </div>
   </div>
-  <!-- -->
-  <div class="col-md-3 " >
-    <div class="box box-solid box-primary" >
-        <div class="box-header">
-            <h3 class="box-title">Registro emprendedores</h3>
-        </div>
-        <div class="box-body" style="min-height:170px">
-            <p>Aplicá a los programas de Financiamiento, Capacitación y Asistencia que existen en el Portal de la Secretaría.
-</p><p>Dentro del Portal podrás consultar la instancia y avance de los mismos</p>
 
-        </div>
-            <div class="box-footer">
-            <a href="{base_url}perfil/registro_emprendedor" class="btn btn-success btn-block">Registrarme</a>
-        </div>
-    </div>
-  </div>
   <!-- -->
   <div class="col-md-3 " >
     <div class="box box-solid box-primary" >
         <div class="box-header">
-            <h3 class="box-title">Registro expertos</h3>
+            <h3 class="box-title">Registro Expertos</h3>
         </div>
         <div class="box-body" style="min-height:170px">
             <p>Registrate como experto y participá del programa Expertos PyME para brindar consultorías y asistencia a las PyMEs</p>
